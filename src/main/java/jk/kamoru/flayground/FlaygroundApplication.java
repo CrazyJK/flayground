376ec722611dddf8c67ecceb1fb389e2a5c4a5fc
kamoru <kamoru@kamoru-mint>
2018-08-27 14:24:08 +0900

    initial import

---
+++ src/main/java/jk/kamoru/flayground/FlaygroundApplication.java
@@ -0,0 +1,12 @@
+package jk.kamoru.flayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlaygroundApplication.class, args);
+	}
+}

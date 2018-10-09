af383796e4b271d167efb082b43ec00e8f6ef36b
kamoru <kamoru@kamoru-home>
2018-10-09 22:02:36 +0900

    add file.encoding

--- FlaygroundStartup.bat
+++ FlaygroundStartup.bat
@@ -5,6 +5,7 @@ setlocal
 set "CURRENT_DIR=%cd%"
 set "FLAYGROUND_HOME=%CURRENT_DIR%"
 set "JAVA_OPTS=%JAVA_OPTS% -Dspring.profiles.active=flay-home"
+set "JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF-8"
 
 echo Using FLAYGROUND_HOME:  %FLAYGROUND_HOME%
 echo Using       JAVA_HOME:  %JAVA_HOME%

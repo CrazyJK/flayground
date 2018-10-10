c795f269726fe5cd403a457edb86936bbdc2ddee
kamoru <kamoru@kamoru-mint>
2018-10-10 14:51:14 +0900

    add candidates announce

--- src/main/java/jk/kamoru/flayground/flay/service/FlayServiceImpl.java
+++ src/main/java/jk/kamoru/flayground/flay/service/FlayServiceImpl.java
@@ -123,6 +123,7 @@ public class FlayServiceImpl implements FlayService {
 		}
 		candiList.clear();
 		FlayFileHandler.rename(flay);
+		notificationService.announce("Accept candidates", flay.getFullname());
 	}
 	
 	@Override

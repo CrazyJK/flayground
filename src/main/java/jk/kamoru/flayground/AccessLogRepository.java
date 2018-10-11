ff9c976cdb82df01c5897ce21ccdb025b09572f0
kamoru <kamoru@kamoru-home>
2018-10-12 00:38:15 +0900

    add controller call count

--- src/main/java/jk/kamoru/flayground/AccessLogRepository.java
+++ src/main/java/jk/kamoru/flayground/AccessLogRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AccessLogRepository extends MongoRepository<AccessLog, String> {
 
+	long countByHandlerInfo(String string);
+
 }
--- src/main/java/jk/kamoru/flayground/FlaygroundController.java
+++ src/main/java/jk/kamoru/flayground/FlaygroundController.java
@@ -21,17 +21,23 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 public class FlaygroundController {
 
 	@Autowired ApplicationContext context;
+	@Autowired AccessLogRepository accessLogRepository;
 
 	@GetMapping("/requestMappingList")
 	public List<Map<String, String>> requestMapping(Model model) {
 		List<Map<String, String>> mappingList = new ArrayList<>();
 		RequestMappingHandlerMapping rmhm = context.getBean(RequestMappingHandlerMapping.class);
 		for (Entry<RequestMappingInfo, HandlerMethod> entry : rmhm.getHandlerMethods().entrySet()) {
+			String beanType = StringUtils.substringAfterLast(entry.getValue().getBeanType().getName(), ".");
+			String beanMethod = entry.getValue().getMethod().getName();
+			long callCount = accessLogRepository.countByHandlerInfo(beanType + "." + beanMethod);
+
 			Map<String, String> mappingData = new HashMap<>();
 			mappingData.put("reqPattern", StringUtils.substringBetween(entry.getKey().getPatternsCondition().toString(), "[", "]"));
 			mappingData.put("reqMethod",  StringUtils.substringBetween(entry.getKey().getMethodsCondition().toString(), "[", "]"));
-			mappingData.put("beanType",   StringUtils.substringAfterLast(entry.getValue().getBeanType().getName(), "."));
-			mappingData.put("beanMethod", entry.getValue().getMethod().getName());
+			mappingData.put("beanType",   beanType);
+			mappingData.put("beanMethod", beanMethod);
+			mappingData.put("callCount", String.valueOf(callCount));
 			mappingList.add(mappingData);
 		}		
 		return mappingList;

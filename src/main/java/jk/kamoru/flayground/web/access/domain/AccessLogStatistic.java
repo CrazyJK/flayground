59b22197a6a2e0a3b4d948acaaa12d0d42851ff9
kamoru <kamoru@kamoru-home>
2018-10-15 23:52:01 +0900

    default constructor

--- src/main/java/jk/kamoru/flayground/web/access/domain/AccessLogStatistic.java
+++ src/main/java/jk/kamoru/flayground/web/access/domain/AccessLogStatistic.java
@@ -7,11 +7,9 @@ import javax.persistence.Id;
 
 import jk.kamoru.flayground.Flayground;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
 public class AccessLogStatistic implements Serializable {
 
 	private static final long serialVersionUID = Flayground.SERIAL_VERSION_UID;
@@ -20,6 +18,8 @@ public class AccessLogStatistic implements Serializable {
     public String handlerInfo;
     public long callCount;
 
+    public AccessLogStatistic() {}
+    
 	public AccessLogStatistic(String handlerInfo) {
 		this.handlerInfo = handlerInfo;
 	}

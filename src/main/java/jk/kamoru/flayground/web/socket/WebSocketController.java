963d450241a1ea345591904fd7e345574d718b49
kamoru <kamoru@kamoru-mint>
2018-12-11 17:49:19 +0900

    websocket ing

--- src/main/java/jk/kamoru/flayground/web/socket/WebSocketController.java
+++ src/main/java/jk/kamoru/flayground/web/socket/WebSocketController.java
@@ -2,17 +2,18 @@ package jk.kamoru.flayground.web.socket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
 
 @RestController
+@RequestMapping("/websocket")
 public class WebSocketController {
 
 	@Autowired WebSocketMessageBrokerStats webSocketMessageBrokerStats;
 
-	@GetMapping("/websocket")
+	@GetMapping("/stats")
 	public String websocketInfo() {
-
 		return webSocketMessageBrokerStats.toString();
 	}
 

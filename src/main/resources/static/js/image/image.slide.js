2fe9c2b5d9c2355cb9f6f00a3f0798121687a42a
kamoru <kamoru@kamoru-home>
2019-03-19 23:55:17 +0900

    pause function, etc

--- src/main/resources/static/js/image/image.slide.js
+++ src/main/resources/static/js/image/image.slide.js
@@ -9,14 +9,25 @@ $(function() {
 	var bgInterval;
 	var bgIntervalTime = LocalStorageItem.getInteger("image.slide.bgIntervalTime", 10);
 	var bgSizeProperties = ['contain', 'cover', 'auto'];
-	var bgSizePropertiesIndex = 0;
+	var bgSizePropertiesIndex = -1;
 	var $image = $("#imageWrap");
 	var $pageProgress = $("#paginationProgress .progress-bar");
+	var $totalNo = $("#totalNo");
+	var $currNo = $("#currNo");
+	var $bgIntervalTime = $("#bgIntervalTime");
+	var $imgPath = $("#imgPath");
+	var $imgTitle = $("#imgTitle");
+	var $imgSize = $("#imgSize");
+	var $imgLength = $("#imgLength");
+	var $imgModified = $("#imgModified");
+	var $bgMode = $("#bgMode");
+	var $pause = $("#pause");
+	var pause = false;
 
 	// setTotalCount
 	Rest.Image.size(function(count) {
 		totalCount = count;
-		$("#totalNo").html(totalCount);
+		$totalNo.html(totalCount);
 
 		// set currIndex by random 
 		currIndex = Random.getInteger(0, totalCount);
@@ -26,7 +37,9 @@ $(function() {
 		$image.css({
 			backgroundSize: bgSizeProperties[++bgSizePropertiesIndex % 3]
 		});
+		$bgMode.html(bgSizeProperties[bgSizePropertiesIndex % 3]);
 	}
+	cssChangeBackgroundSize();
 
 	// navigate
 	$image.navEvent(function(signal, e) {
@@ -50,31 +63,44 @@ $(function() {
 	});
 	
 	// new index
-	$("#currNo").on("keyup", function(e) {
+	$currNo.on("keyup", function(e) {
+	    e.stopPropagation();
 		if (e.keyCode === 13) {
 			control.jump(parseInt($(this).val().replace(/[^0-9]/g, '')));
 		}
 	});
 	
 	// new interval time
-	$("#bgIntervalTime").on("keyup", function(e) {
+	$bgIntervalTime.on("keyup", function(e) {
+	    e.stopPropagation();
 		if (e.keyCode === 13) {
 			bgIntervalTime = parseInt($(this).val().replace(/[^0-9]/g, ''));
 			LocalStorageItem.set("image.slide.bgIntervalTime", bgIntervalTime);
 			view();
+			$("#notice").html('set interval ' + bgIntervalTime + 's');
+			setTimeout(function() {
+				$("#notice").html('');
+			}, 1500);
 		}
 	}).val(bgIntervalTime + "s");
 	
 	// open explorer
-	$("#imgPath").on("click", function() {
-		Rest.Flay.openFolder($(this).dats("path"));
+	$imgPath.on("click", function() {
+		Rest.Flay.openFolder($(this).data("path"));
 	}).css("cursor", "pointer");
 	
 	// popup image
-	$("#imgTitle").on("click", function() {
+	$imgTitle.on("click", function() {
 		Popup.imageByNo(currIndex);
 	}).css("cursor", "pointer");
 
+	// slide pause
+	$pause.on("click", function() {
+		pause = $(this).toggleClass("active").hasClass("active");
+		console.log('pause', pause);
+		view();
+	}).css("cursor", "pointer");
+	
 	var control = {
 			jump: function(idx) {
 				currIndex = idx;
@@ -111,15 +137,15 @@ $(function() {
 				$pageProgress.css({
 					width: ((currIndex + 1) / totalCount * 100).toFixed(1) + "%"
 				});
-				$("#currNo").val(currIndex);
-				$("#imgSize").html(this.naturalWidth + ' x ' + this.naturalHeight);
+				$currNo.val(currIndex);
+				$imgSize.html(this.naturalWidth + ' x ' + this.naturalHeight);
 
 				// get info
 				Rest.Image.get(currIndex, function(imgInfo) {
-					$("#imgPath").html(imgInfo.path.replace(/\\/gi, '/').split('/').pop()).data("path", imgInfo.path);
-					$("#imgTitle").html(imgInfo.name);
-					$("#imgLength").html(File.formatSize(imgInfo.length));
-					$("#imgModified").html(new Date(imgInfo.modified).format('yyyy-MM-dd'));
+					$imgPath.html(imgInfo.path.replace(/\\/gi, '/').split('/').pop()).data("path", imgInfo.path);
+					$imgTitle.html(imgInfo.name);
+					$imgLength.html(File.formatSize(imgInfo.length));
+					$imgModified.html(new Date(imgInfo.modified).format('yyyy-MM-dd'));
 				});
 			};
 			img.src = PATH + "/static/image/" + currIndex;
@@ -128,10 +154,13 @@ $(function() {
 		
 		clearInterval(bgInterval);
 		show();
-		bgInterval = setInterval(function() {
-			currIndex++;
-			show();
-		}, 1000 * bgIntervalTime);
+		if (!pause) {
+			bgInterval = setInterval(function() {
+				currIndex++;
+				show();
+			}, 1000 * bgIntervalTime);
+			console.log('setInterval');
+		}
 	};
 
 	view();

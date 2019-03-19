7d0d7ad0c4dfbfe6e14901b56cf222f626b6cee9
namjk <namjk@10.30.20.87>
2019-03-19 20:03:42 +0900

    simplify

--- src/main/resources/static/js/image/image.slide.js
+++ src/main/resources/static/js/image/image.slide.js
@@ -3,84 +3,77 @@
  */
 
 
-var slide = (function() {
+$(function() {
 	var totalCount = 0;
 	var currIndex  = 0;
 	var bgInterval;
 	var bgIntervalTime = LocalStorageItem.getInteger("image.slide.bgIntervalTime", 10);
 	var bgSizeProperties = ['contain', 'cover', 'auto'];
 	var bgSizePropertiesIndex = 0;
-	var $image;
-	var $pageProgress;
+	var $image = $("#imageWrap");
+	var $pageProgress = $("#paginationProgress .progress-bar");
 
-	var init = function() {
-		$image = $("#imageWrap");
-		$pageProgress = $("#paginationProgress .progress-bar");
-		
-		// setTotalCount
-		Rest.Image.size(function(count) {
-			totalCount = count;
-			$("#totalNo").html(totalCount);
-		});
-		
+	// setTotalCount
+	Rest.Image.size(function(count) {
+		totalCount = count;
+		$("#totalNo").html(totalCount);
+
 		// set currIndex by random 
 		currIndex = Random.getInteger(0, totalCount);
-	};
+	});
 
-	var addEventListener = function() {
-		function cssChangeBackgroundSize() {
-			$image.css({
-				backgroundSize: bgSizeProperties[++bgSizePropertiesIndex % 3]
-			});
+	function cssChangeBackgroundSize() {
+		$image.css({
+			backgroundSize: bgSizeProperties[++bgSizePropertiesIndex % 3]
+		});
+	}
+
+	// navigate
+	$image.navEvent(function(signal, e) {
+		console.log('e.keyCode', signal);
+		switch (signal) {
+		case 32: // key space
+			control.random();
+			break;
+		case 37: // key left
+		case 1: // mousewheel up
+			control.prev();
+			break;
+		case 39: // right
+		case -1: // mousewheel down
+			control.next();
+			break;
+		case 1002:
+			cssChangeBackgroundSize();
+			break;
 		}
-
-		// navigate
-		$image.navEvent(function(signal, e) {
-//			console.log('e.keyCode', signal);
-			switch (signal) {
-			case 32: // key space
-				control.random();
-				break;
-			case 37: // key left
-			case 1: // mousewheel up
-				control.prev();
-				break;
-			case 39: // right
-			case -1: // mousewheel down
-				control.next();
-				break;
-			case 1002:
-				cssChangeBackgroundSize();
-				break;
-			}
-		});
-		
-		// new index
-		$("#currNo").on("keyup", function(e) {
-			if (e.keyCode === 13) {
-				control.jump(parseInt($(this).val().replace(/[^0-9]/g, '')));
-			}
-		});
-		
-		// new interval time
-		$("#bgIntervalTime").on("keyup", function(e) {
-			if (e.keyCode === 13) {
-				bgIntervalTime = parseInt($(this).val().replace(/[^0-9]/g, ''));
-				LocalStorageItem.set("image.slide.bgIntervalTime", bgIntervalTime);
-				view();
-			}
-		}).val(bgIntervalTime + "s");
-		
-		// open explorer
-		$("#imgPath").on("click", function() {
-			Rest.Flay.openFolder($(this).dats("path"));
-		}).css("cursor", "pointer");
-		
-		// popup image
-		$("#imgTitle").on("click", function() {
-			Popup.imageByNo(currIndex);
-		}).css("cursor", "pointer");
-	};
+	});
+	
+	// new index
+	$("#currNo").on("keyup", function(e) {
+		if (e.keyCode === 13) {
+			control.jump(parseInt($(this).val().replace(/[^0-9]/g, '')));
+		}
+	});
+	
+	// new interval time
+	$("#bgIntervalTime").on("keyup", function(e) {
+		if (e.keyCode === 13) {
+			bgIntervalTime = parseInt($(this).val().replace(/[^0-9]/g, ''));
+			LocalStorageItem.set("image.slide.bgIntervalTime", bgIntervalTime);
+			view();
+		}
+	}).val(bgIntervalTime + "s");
+	
+	// open explorer
+	$("#imgPath").on("click", function() {
+		Rest.Flay.openFolder($(this).dats("path"));
+	}).css("cursor", "pointer");
+	
+	// popup image
+	$("#imgTitle").on("click", function() {
+		Popup.imageByNo(currIndex);
+	}).css("cursor", "pointer");
 
 	var control = {
 			jump: function(idx) {
@@ -108,8 +101,8 @@ var slide = (function() {
 			} else if (currIndex < 0) {
 				currIndex = totalCount - 1;
 			}
-//			console.log('image.show', currIndex);
-			
+			console.log('image.show', currIndex);
+			 
 			var img = new Image();
 			img.onload = function() {
 				$image.css({
@@ -130,7 +123,7 @@ var slide = (function() {
 				});
 			};
 			img.src = PATH + "/static/image/" + currIndex;
-//			console.log('img.src', PATH, "/static/image/", currIndex);
+			console.log('img.src', PATH, "/static/image/", currIndex);
 		}
 		
 		clearInterval(bgInterval);
@@ -140,17 +133,7 @@ var slide = (function() {
 			show();
 		}, 1000 * bgIntervalTime);
 	};
-	
-	return {
-		start: function() {
-			init();
-			addEventListener();
-			view();
-		}
-	};
-	
-}());
 
-$(function() {
-	slide.start();
+	view();
+
 });

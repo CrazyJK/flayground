4695d79814666924b0264e94cd80764a804b533d
kamoru <kamoru@kamoru-home>
2019-04-20 13:34:35 +0900

    find actress picture in imagesource

--- src/main/java/jk/kamoru/flayground/info/source/ActressInfoSource.java
+++ src/main/java/jk/kamoru/flayground/info/source/ActressInfoSource.java
@@ -1,32 +1,26 @@
 package jk.kamoru.flayground.info.source;
 
 import java.io.File;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.annotation.PostConstruct;
-
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import jk.kamoru.flayground.FlayProperties;
 import jk.kamoru.flayground.Flayground;
+import jk.kamoru.flayground.image.domain.Image;
+import jk.kamoru.flayground.image.source.ImageSource;
 import jk.kamoru.flayground.info.domain.Actress;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@Repository
 public class ActressInfoSource extends InfoSourceJsonAdapter<Actress, String> {
 
 	@Autowired FlayProperties flayProperties;
 
-	Collection<File> coverPool;
+	@Autowired ImageSource<Image> imageSource;
 
 	@Override
 	File getInfoFile() {
@@ -36,7 +30,8 @@ public class ActressInfoSource extends InfoSourceJsonAdapter<Actress, String> {
 	@Override
 	public Actress get(String name) {
 		Actress actress = super.get(name);
-		actress.setCovers(findCoverFile(name));
+		if (actress.getCovers() == null)
+			actress.setCovers(findCoverFile(name));
 		return actress;
 	}
 
@@ -51,25 +46,15 @@ public class ActressInfoSource extends InfoSourceJsonAdapter<Actress, String> {
 	}
 
 	private List<File> findCoverFile(String name) {
-		if (coverPool.size() > 0) {
-			Supplier<Stream<File>> supplier = () -> coverPool.stream().filter(f -> {
-				return f.getName().startsWith(name);
-			});
-			long count = supplier.get().count();
-			if (count == 0) {
-				return null;
-			} else {
-				return supplier.get().collect(Collectors.toList());
-			}
-		} else {
+		Supplier<Stream<Image>> supplier = () -> imageSource.list().stream().filter(i -> {
+			return i.getName().startsWith(name);
+		});
+		long count = supplier.get().count();
+		if (count == 0) {
 			return null;
+		} else {
+			return supplier.get().map(Image::getFile).collect(Collectors.toList());
 		}
-	}
-
-	@PostConstruct
-	void loadCover() {
-		coverPool = FileUtils.listFiles(flayProperties.getInfoPath(), Flayground.FILE.IMAGE_SUFFIXs, true);
-		log.info(String.format("%5s actress cover", coverPool.size()));
 	}
 
 }

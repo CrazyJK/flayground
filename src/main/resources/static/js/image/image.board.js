bf29e91644e2c7ac56cd4f0f48df8eada6ee9681
kamoru <kamoru@kamoru-home>
2019-09-19 00:04:16 +0900

    board 0918

--- src/main/resources/static/js/image/image.board.js
+++ src/main/resources/static/js/image/image.board.js
@@ -4,23 +4,22 @@
 
 $(function() {
 	var totalCount = 0;
-	var currIndex  = 0;
 	var bgIntervalTime = LocalStorageItem.getInteger("image.board.bgIntervalTime", 10);
 	var bgInterval;
-	var pause = false;
+	var pause  = false;
 	var tile   = LocalStorageItem.getBoolean("image.board.tile",   false);
 	var random = LocalStorageItem.getBoolean("image.board.random", false);
 	var rotate = LocalStorageItem.getBoolean("image.board.rotate", false);
-	var DISPLAY_WIDTH_COUNT = 4;
-	var DISPLAY_HEIGHT_COUNT = 5;
-	var DISPLAY_COUNT = DISPLAY_WIDTH_COUNT * DISPLAY_HEIGHT_COUNT + 1;
-	var displaySequence = 0;
-	var POSITION_OFFSET = 100;
+	var x = 5, y = 4;
+	var TILE_ROW_COUNT = 0;
+	var TILE_COL_COUNT = 0;
+	var POSITION_MARGIN = 100;
 	var ROTATE_LIMIT = 10;
+	var displaySequence = 0;
 
-	var $imageWrap = $("#imageWrap");
+	var $imageWrap  = $("#imageWrap");
 	var $controlBox = $("#controlBox");
-	var $progress = $("#paginationProgress > .progress-bar");
+	var $progress   = $("#paginationProgress > .progress-bar");
 
 	$imageWrap.navEvent(function(signal, e) {
 		// console.log('e.keyCode', signal);
@@ -39,11 +38,16 @@ $(function() {
 		case 1001: // mouse left click
 			break;
 		case 1002: // mouse middle click
-			shuffle();
+			control.shuffle();
 			break;
 		}
 	});
 
+	$(window).on("resize", function() {
+		TILE_ROW_COUNT = window.innerWidth > window.innerHeight ? x : y;
+		TILE_COL_COUNT = window.innerWidth > window.innerHeight ? y : x;
+	}).trigger("resize");
+	
 	$controlBox.on('init', function() {
 		// console.log('$controlBox init');
 		Rest.Image.size(function(count) {
@@ -58,15 +62,19 @@ $(function() {
 
 	}).on('setInfo', function(e, image, imgInfo) {
 		$("#imgPath").html(imgInfo.path.replace(/\\/gi, '/').split('/').pop()).data("path", imgInfo.path);
-		$("#imgTitle").html(imgInfo.name);
+		$("#imgTitle").html(imgInfo.name).data("idx", imgInfo.idx);
 		$("#imgSize").html(image.naturalWidth + ' x ' + image.naturalHeight);
 		$("#imgLength").html(File.formatSize(imgInfo.length));
 		$("#imgModified").html(new Date(imgInfo.modified).format('yyyy-MM-dd'));
-		$("#currNo").val(currIndex);
+		$("#currNo").val(imgInfo.idx);
+		$progress.css({
+			width: ((imgInfo.idx + 1) / totalCount * 100).toFixed(1) + "%"
+		});
+		$("#infoMsg").html(displaySequence);
 	}).on('notice', function(e, msg) {
 		var $span = $("<span>", {class: 'msgBox'}).html(msg).appendTo($("#notice"));
 		setTimeout(function() {
-			$span.hide('blind', {direction: 'right'}, 500, function() {
+			$span.hide('blind', {direction: 'down'}, 300, function() {
 				$(this).remove();
 			});
 		}, 1500);
@@ -74,20 +82,21 @@ $(function() {
 		Rest.Flay.openFolder($(this).data("path"));
 		$controlBox.trigger('notice', 'open folder: ' + $(this).data("path"));
 	}).on('click', '#imgTitle', function() {
-		Popup.imageByNo(currIndex);
-		$controlBox.trigger('notice', 'pupup image: ' + currIndex);
+		var clickedImageIndex = parseInt($(this).data("idx"));
+		Popup.imageByNo(clickedImageIndex);
+		$controlBox.trigger('notice', 'pupup image: ' + clickedImageIndex);
 	}).on('keyup', '#bgIntervalTime', function(e) {
 		e.stopPropagation();
 		if (e.keyCode === 13) {
 			bgIntervalTime = parseInt($(this).val().replace(/[^0-9]/g, ''));
 			LocalStorageItem.set("image.board.bgIntervalTime", bgIntervalTime);
 			$controlBox.trigger('notice', 'set interval ' + bgIntervalTime + 's');
-			view();
+			control.next();
 		}
 	}).on('click', '#pause', function() {
 		pause = $(this).toggleClass("active").hasClass("active");
 		$controlBox.trigger('notice', 'slide pause: ' + pause);
-		view();
+		control.next();
 	}).on('click', '#random', function() {
 		random = $(this).toggleClass("active").hasClass("active");
 		LocalStorageItem.set("image.board.random", random);
@@ -96,125 +105,113 @@ $(function() {
 		rotate = $(this).toggleClass("active").hasClass("active");
 		LocalStorageItem.set("image.board.rotate", rotate);
 		$controlBox.trigger('notice', 'rotate: ' + rotate);
-		setRotate();
+		toggleRotate();
 	}).on('click', '#tile', function() {
 		tile = $(this).toggleClass("active").hasClass("active");
 		LocalStorageItem.set("image.board.tile", tile);
 		$controlBox.trigger('notice', 'tile mode: ' + tile);
-		setPosition();
+		toggleTile();
 	}).on('keyup', '#currNo', function(e) {
 		e.stopPropagation();
 		if (e.keyCode === 13) {
-			control.jump(parseInt($(this).val().replace(/[^0-9]/g, '')));
-			$controlBox.trigger('notice', 'go slide: ' + currIndex);
+			var wantedImageIndex = parseInt($(this).val().replace(/[^0-9]/g, ''));
+			control.jump(wantedImageIndex);
+			$controlBox.trigger('notice', 'go slide: ' + wantedImageIndex);
 		}
-	});
-
-	$progress.on('progress', function() {
-		$(this).css({
-			width: ((currIndex + 1) / totalCount * 100).toFixed(1) + "%"
-		});
 	});
 
 	var control = {
 			jump: function(idx) {
-				currIndex = idx;
-				view();
+				view(idx);
 			},
 			random: function() {
-				currIndex = Random.getInteger(0, totalCount);
-				view();
-			},
-			prev: function() {
-				currIndex--;
-				back();
+				view(Random.getInteger(0, totalCount));
 			},
 			next: function() {
-				currIndex++;
-				view();
+				view(parseInt($("#currNo").val()) + 1);
+			},
+			prev: function() { // delete last image
+				$imageWrap.children(":last-child").remove();
+				if ($imageWrap.children(":last-child").length > 0) {
+					var lastImgData = $imageWrap.children(":last-child").addClass("active").data("data");
+					$controlBox.trigger('setInfo', [lastImgData.image, lastImgData.info]);
+				}
+			},
+			shuffle: function() {
+				if (tile) {
+					toggleTile();
+				} else {
+					$imageWrap.children().each(function(idx) {
+						var imgData = $(this).data("data");
+						imgData.css = getImageExteriorCss(imgData.image);
+						$(this).css(imgData.css);
+					});
+					$imageWrap.children(':nth-child(' + Random.getInteger(0, $imageWrap.children().length) + ')').click();
+				}
 			}
 	};
 
-	var back = function() {
-		$imageWrap.children(":last-child").remove();
-		if ($imageWrap.children(":last-child").length > 0) {
-			var lastImgData = $imageWrap.children(":last-child").addClass("active").data("data");
-			currIndex = lastImgData.index;
-			$controlBox.trigger('setInfo', [lastImgData.self, lastImgData.info]);
-		}
-	};
-	
-	var shuffle = function() {
-		if (!tile) {
-			$imageWrap.children().each(function(idx) {
-				var imgData = $(this).data("data");
-				var imgTop  = Random.getInteger(0 - POSITION_OFFSET, window.innerHeight - $(this).height() + POSITION_OFFSET);
-				var imgLeft = Random.getInteger(0 - POSITION_OFFSET, window.innerWidth  - $(this).width()  + POSITION_OFFSET);
-				// console.log(idx, imgTop, imgLeft);
-				$(this).css({
-					top: imgTop,
-					left: imgLeft,
-				});
-				imgData.top = imgTop;
-				imgData.left = imgLeft;
-			});
-			$imageWrap.children(':nth-child(' + Random.getInteger(0, $imageWrap.children().length) + ')').click();
-		} else {
-			setPosition();
-		}
-	};
-	
-	var setPosition = function() {
+	var toggleTile = function() {
+		var gridWidth  = Math.round(window.innerWidth  / TILE_ROW_COUNT);
+		var gridHeight = Math.round(window.innerHeight / TILE_COL_COUNT);
+		var gridRatio  = gridWidth / gridHeight;
+		// console.log("toggleTile", gridWidth, gridHeight, gridRatio);
 		$imageWrap.children().each(function(idx) {
 			var data = $(this).data("data");
-			var displaySeq = data.displaySeq % 20;
 			if (tile) {
+				var displaySeq = data.displaySeq % (TILE_ROW_COUNT * TILE_COL_COUNT);
+				var imageRatio = data.image.naturalWidth / data.image.naturalHeight;
 				$(this).addClass("tile").css({
-					top: Math.floor(displaySeq / DISPLAY_WIDTH_COUNT) * (window.innerHeight / DISPLAY_HEIGHT_COUNT),
-					left: (displaySeq % DISPLAY_WIDTH_COUNT) * (window.innerWidth / DISPLAY_WIDTH_COUNT),
+					top: Math.floor(displaySeq / TILE_ROW_COUNT) * gridHeight,
+					left: (displaySeq % TILE_ROW_COUNT) * gridWidth,
+					width: imageRatio > gridRatio ? gridWidth - 16 : 'initial',
+					height: imageRatio > gridRatio ? 'initial' : gridHeight - 16
 				});
-				if (data.self.naturalHeight > data.self.naturalWidth) {
-					$(this).css({
-						width: 'initial',
-						height: window.innerHeight / DISPLAY_HEIGHT_COUNT - 16,
-						maxWidth: window.innerWidth / DISPLAY_WIDTH_COUNT - 16,
-						maxHeight: 'initial'
-					});
-				} else {
-					$(this).css({
-						width: window.innerWidth / DISPLAY_WIDTH_COUNT - 16,
-						height: 'initial',
-						maxWidth: 'initial',
-						maxHeight: window.innerHeight / DISPLAY_HEIGHT_COUNT - 16
-					});
-				}
 			} else {
-				$(this).removeClass("tile").css({
-					top: data.top,
-					left: data.left,
-					width: data.width,
-					height: data.height,
-					maxWidth: 'initial',
-					maxHeight: 'initial'
-				});
+				$(this).removeClass("tile").css(data.css);
 			}
 		});
 	};
 	
-	var setRotate = function() {
+	var toggleRotate = function() {
 		$imageWrap.children().each(function(idx) {
-			var degree = rotate ? Random.getInteger(ROTATE_LIMIT * -1, ROTATE_LIMIT) : 0;
 			$(this).css({
-				transform: 'rotate(' + degree + 'deg)'
+				transform: 'rotate(' + (rotate ? Random.getInteger(ROTATE_LIMIT * -1, ROTATE_LIMIT) : 0) + 'deg)'
 			});
 		});
 	};
 
-	var view = function() {
+	var getImageExteriorCss = function(image) {
+		var factor = 1.0; // determine size factor
+		if (window.innerHeight < image.naturalHeight || window.innerWidth < image.naturalWidth) {
+			factor = Math.min(window.innerHeight / image.naturalHeight, window.innerWidth / image.naturalWidth).toFixed(2);
+			if (factor > 0.1) {
+				factor -= 0.1;
+			}
+		}
+		var imgHeight = image.naturalHeight * factor;
+		var imgWidth  = image.naturalWidth  * factor;
+		var imgTop  = Random.getInteger(0 - POSITION_MARGIN, window.innerHeight - imgHeight + POSITION_MARGIN);
+		var imgLeft = Random.getInteger(0 - POSITION_MARGIN, window.innerWidth  - imgWidth  + POSITION_MARGIN);
+		var degree = rotate ? Random.getInteger(ROTATE_LIMIT * -1, ROTATE_LIMIT) : 0;
+		return {
+			top: imgTop,
+			left: imgLeft,
+			width: imgWidth,
+			height: imgHeight,
+			opacity: 1,
+			transform: 'rotate(' + degree + 'deg)'
+		};
+	};
+	
+	var view = function(currIndex) {
 		function show() {
 			if (pause) {
 				return;
 			}
+			// Stop nav event
+			$imageWrap.navActive(false);
+
 			// decision current index
 			if (random) {
 				currIndex = Random.getInteger(0, totalCount);
@@ -226,122 +223,85 @@ $(function() {
 				}
 			}
 
+			LocalStorageItem.set("image.board.index", currIndex);
+			
 			// old picture remove
-			if ($imageWrap.children().length >= DISPLAY_COUNT) {
+			if ($imageWrap.children().length >= TILE_ROW_COUNT * TILE_COL_COUNT + 1) {
 				$imageWrap.children(":first-child").remove();
 			}
 
 			$imageWrap.children().removeClass("active").draggable("disable");
-			setPosition();
+			toggleTile();
 
 			// get info
 			Rest.Image.get(currIndex, function(info) {
-				// get Image
+				// load Image
 				var image = new Image();
 				image.onload = function() {
 					var _self = this;
-					var imgTop = 0,
-						imgLeft = 0,
-						imgWidth = 0,
-						imgHeight = 0,
-						factor = 1.0, 
-						degree = rotate ? Random.getInteger(ROTATE_LIMIT * -1, ROTATE_LIMIT) : 0;
-
-					// determine size factor
-					if (window.innerHeight < _self.naturalHeight || window.innerWidth < _self.naturalWidth) {
-						factor = Math.min(window.innerHeight / _self.naturalHeight, window.innerWidth / _self.naturalWidth).toFixed(2);
-						if (factor > 0.1) {
-							factor -= 0.1;
-						}
-					}
-					imgHeight = _self.naturalHeight * factor;
-					imgWidth  = _self.naturalWidth  * factor;
-					imgTop  = Random.getInteger(0 - POSITION_OFFSET, window.innerHeight - imgHeight + POSITION_OFFSET);
-					imgLeft = Random.getInteger(0 - POSITION_OFFSET, window.innerWidth  - imgWidth  + POSITION_OFFSET);
-					// console.log("factor", factor, "imgTop", imgTop, "imgLeft", imgLeft, "imgWidth", imgWidth, "imgHeight", imgHeight,  "degree", degree);
 					
-					var $img = $("<img>", {src: _self.src, class: 'board-image active'}).css({
-						top: imgTop,
-						left: imgLeft,
-						width: imgWidth,
-						height: imgHeight,
-						opacity: 0
-					}).data("data", {
-						index: currIndex,
-						self: _self,
-						info: info,
+					var decisionCss = getImageExteriorCss(_self);
+					var initialCss = Object.assign({}, decisionCss);
+					initialCss.opacity = 0;
+					initialCss.transform = 'none';
+					
+					var $img = $("<img>", {src: _self.src, class: 'board-image active'}).css(initialCss).data("data", {
 						displaySeq: displaySequence++,
-						top: imgTop,
-						left: imgLeft,
-						width: imgWidth,
-						height: imgHeight,
+						image: _self,
+						info: info,
+						css: decisionCss
 					}).appendTo($imageWrap).animate({
 						opacity: 1
 					}, 300, function() {
-						$(this).css({
-							transform: 'rotate(' + degree + 'deg)'
-						});
+						$(this).css(decisionCss);
+						// Start nav event
+						$imageWrap.navActive(true);
 					}).on("click", function() {
 						// console.log("click", $(this).next().length);
 						if ($(this).next().length > 0) {
 							$imageWrap.children().removeClass("active").draggable("disable");
-							setPosition();
+							toggleTile();
 
 							var data = $(this).data("data");
 							$(this).appendTo($imageWrap).animate({
 								opacity: 1
 							}, 100, function() {
-								$(this).removeClass("tile").css({
-									top: data.top,
-									left: data.left,
-									width: data.width,
-									height: data.height,
-									maxWidth: 'initial',
-									minWidth: 'initial',
-									transform: 'rotate(' + (rotate ? Random.getInteger(ROTATE_LIMIT * -1, ROTATE_LIMIT) : 0) + 'deg)'
-								}).addClass("active");
-							}).draggable("enable");
+								$(this).removeClass("tile").addClass("active").css(data.css).draggable("enable");
+							});
 							
-							currIndex = data.index;
-							$controlBox.trigger('setInfo', [data.self, data.info]);
+							$controlBox.trigger('setInfo', [data.image, data.info]);
 						}
 					}).dblclick(function() {
 						// console.log("dblclick");
 						var data = $(this).data("data");
-						Popup.imageByNo(data.index);
+						Popup.imageByNo(data.info.idx);
 					}).draggable({
 						stop: function(event, ui) {
 							// console.log(event, ui);
-							var _data = $(event.target).data("data");
-							_data.top = ui.position.top;
-							_data.left = ui.position.left;
+							var data = $(event.target).data("data");
+							data.css.top  = ui.position.top;
+							data.css.left = ui.position.left;
 						}
 					});
 					
-					$progress.trigger('progress');
-
 					$controlBox.trigger('setInfo', [_self, info]);
 				};
 				image.src = PATH + "/static/image/" + currIndex;
-
 			});
-			
 		}
 
 		clearInterval(bgInterval);
 		show();
 		if (!pause) {
 			bgInterval = setInterval(function() {
-				currIndex++;
-				show();
+				control.next();
 			}, 1000 * bgIntervalTime);
-			// console.log('setInterval', bgIntervalTime);
 		}
 	};
 
 	$controlBox.trigger('init');
 
-	control.random();
+	control.jump(LocalStorageItem.getInteger("image.board.index", 0));
 
 	$("body").toggleClass("bg-dark", LocalStorageItem.get('flay.bgtheme', 'D') === 'D').css({backgroundColor: LocalStorageItem.get('flay.bgcolor')});
 });

92b409ca50cce5181ef4d488f219f575ded4c36b
kamoru <kamoru@kamoru-home>
2020-10-17 21:13:24 +0900

    java.awt.headless

--- FlaygroundStartup.bat
+++ FlaygroundStartup.bat
@@ -6,6 +6,7 @@ set "CURRENT_DIR=%cd%"
 set "FLAYGROUND_HOME=%CURRENT_DIR%"
 set "JAVA_OPTS=%JAVA_OPTS% -Dspring.profiles.active=flay-home"
 set "JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF-8"
+set "JAVA_OPTS=%JAVA_OPTS% -Djava.awt.headless=true"
 
 echo Using FLAYGROUND_HOME: %FLAYGROUND_HOME%
 echo Using       JAVA_HOME: %JAVA_HOME%

12499a5528ac0a94d046f06e93e81ab7309c3a78
jk kamoru <Crazy.4.JK@gmail.com>
2021-09-18 00:15:17 +0900

    startup

--- FlaygroundStartup.sh
+++ FlaygroundStartup.sh
@@ -2,15 +2,16 @@
 
 CURRENT_DIR=${PWD}
 FLAYGROUND_HOME="$CURRENT_DIR"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseStringDeduplication"
+JAVA_OPTS="$JAVA_OPTS -Dserver.port=80"
 JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=flay-wsl"
 JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8"
 JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true"
 JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true"
-JAVA_OPTS="$JAVA_OPTS -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseStringDeduplication"
 JAVA_OPTS="$JAVA_OPTS -Dlogging.file.name=flayground.log"
 
 echo "Using FLAYGROUND: $FLAYGROUND_HOME"
 echo "Using JAVA_HOME:  $JAVA_HOME"
 echo "Using JAVA_OPTS:  $JAVA_OPTS"
 
-$JAVA_HOME/bin/java $JAVA_OPTS -jar $FLAYGROUND_HOME/Flayground.jar
+$JAVA_HOME/bin/java $JAVA_OPTS -jar $FLAYGROUND_HOME/target/Flayground.jar

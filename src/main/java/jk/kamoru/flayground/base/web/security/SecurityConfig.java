131412f87defbbdf06b0a95960a999b584dc9a26
jongkwan-nam <jongkwan.nam@gmail.com>
2022-07-21 14:45:38 +0900

    enhance

--- src/main/java/jk/kamoru/flayground/base/web/security/SecurityConfig.java
+++ src/main/java/jk/kamoru/flayground/base/web/security/SecurityConfig.java
@@ -3,10 +3,11 @@ package jk.kamoru.flayground.base.web.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -15,6 +16,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import jk.kamoru.flayground.FlayProperties;
 import jk.kamoru.flayground.base.web.security.authentication.FlayAuthenticationFailureHandler;
 import jk.kamoru.flayground.base.web.security.authentication.FlayAuthenticationSuccessHandler;
@@ -22,48 +24,51 @@ import jk.kamoru.flayground.base.web.security.filter.AutomaticallyAuthentication
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-	@Autowired FlayProperties flayProperties;
+	@Autowired
+	FlayProperties flayProperties;
 
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 				.withUser("admin").password("{noop}6974").roles("ADMIN") // {noop} for plain text
 				.and()
 				.withUser("kamoru").password("{noop}3806").roles("USER");
+		return auth.build();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.headers()
 				.frameOptions().sameOrigin()
 				.and()
-			.csrf()
+				.csrf()
 				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 				.and()
-			.cors()
+				.cors()
 				.configurationSource(getCorsConfigurationSource())
 				.and()
-			.authorizeRequests()
+				.authorizeRequests()
 				.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
 				.antMatchers("/", "/favicon.ico", "/index.html", "/webjars/**").permitAll()
 				.antMatchers("/batch/**").hasRole("ADMIN")
 				.anyRequest().authenticated()
 				.and()
-			.formLogin()
+				.formLogin()
 				.loginPage("/dist/login.html").permitAll()
 				.successHandler(getFlayAuthenticationSuccessHandler())
 				.failureHandler(getFlayAuthenticationFailureHandler())
 				.and()
-			.rememberMe()
+				.rememberMe()
 				.rememberMeParameter("remember-me")
 				.and()
-			.logout()
+				.logout()
 				.logoutSuccessUrl("/")
 				.and()
-			.addFilterBefore(automaticallyAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-			.httpBasic();
+				.addFilterBefore(automaticallyAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.httpBasic();
+		return http.build();
 	}
 
 	@Bean
@@ -71,12 +76,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new AutomaticallyAuthenticationFilter(flayProperties.getAutomaticallyCertificatedIp());
 	}
 
-    @Bean
+	@Bean
 	public AuthenticationSuccessHandler getFlayAuthenticationSuccessHandler() {
 		return new FlayAuthenticationSuccessHandler();
 	}
 
-    @Bean
+	@Bean
 	public AuthenticationFailureHandler getFlayAuthenticationFailureHandler() {
 		return new FlayAuthenticationFailureHandler();
 	}

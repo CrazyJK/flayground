475e4ce1163086a0224ccddfe5dab5b6e2cf4ce1
jk kamoru <Crazy.4.JK@gmail.com>
2022-10-03 21:57:37 +0900

    position

--- src/main/java/jk/kamoru/flayground/FlayException.java
+++ src/main/java/jk/kamoru/flayground/FlayException.java
@@ -4,24 +4,20 @@ public class FlayException extends RuntimeException {
 
   private static final long serialVersionUID = Flayground.SERIAL_VERSION_UID;
 
-  public FlayException() {
-    super();
+  public FlayException(String message) {
+    super(message);
   }
 
-  public FlayException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);
+  public FlayException(Throwable cause) {
+    super(cause);
   }
 
   public FlayException(String message, Throwable cause) {
     super(message, cause);
   }
 
-  public FlayException(String message) {
-    super(message);
-  }
-
-  public FlayException(Throwable e) {
-    super(e);
+  public FlayException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
   }
 
 }

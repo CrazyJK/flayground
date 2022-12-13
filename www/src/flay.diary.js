255068eb2bd34d9bb88fa37167991a9533342f17
jk kamoru <Crazy.4.JK@gmail.com>
2022-12-14 00:17:47 +0900

    달력 추가

--- www/src/flay.diary.js
+++ www/src/flay.diary.js
@@ -14,7 +14,6 @@ const weatherMap = {
   snowy: 'snowflake-o',
 };
 
-const diaryList = document.querySelector('#diaryList > .diary-list');
 const diaryTitle = document.querySelector('#diaryTitle');
 const diaryDate = document.querySelector('#diaryDate');
 const diaryDay = document.querySelector('#diaryDay');
@@ -42,85 +41,167 @@ const diaryEditor = new Editor({
 });
 diaryEditor.hide();
 
-loadDiaryList();
+renderCalendar();
+markDiaryDates();
+addCalendarEventListener();
 
 /**
- * 일기 목록 로딩
+ * 화면에 달력 표현
  */
-function loadDiaryList() {
-  /**
-   * 일기 목록에 요소 추가
-   * @param {*} diary
-   */
-  function appendDiary(diary) {
-    function getDay() {
-      return new Intl.DateTimeFormat('ko-KR', { weekday: 'short' }).format(new Date(diary.date));
-    }
-    const labelElement = document.createElement('label');
-    labelElement.textContent = diary.new ? 'new Diary' : diary.date.substring(2, 10) + ' (' + getDay() + ')';
-    labelElement.setAttribute('date', diary.date);
-    labelElement.setAttribute('title', diary.title);
-    labelElement.addEventListener('click', (e) => {
-      console.log('click Diary label', diary);
+function renderCalendar() {
+  const renderingMonthSize = Math.floor(document.querySelector('#diaryCal').clientWidth / (14 * 16));
+  const today = new Date();
+  const todayYear = today.getFullYear();
+  const todayMonth = today.getMonth();
+  const todayDay = today.getDate();
 
-      // 클릭 이벤트에서 label node 찾기
-      let clickedLabel;
-      if (e.target.nodeName === 'LABEL') {
-        clickedLabel = e.target;
-      } else if (e.target.nodeName === 'I') {
-        clickedLabel = e.target.parentNode;
-      } else {
-        throw Error('not found label');
-      }
+  let html = '';
+  for (let r = 0; r < renderingMonthSize; r++) {
+    console.log('rendering', r);
 
-      // 중복 클릭 방지
-      if (clickedLabel.classList.contains('active')) {
-        return;
-      }
+    const refDate = new Date(today.getFullYear(), today.getMonth() - r, today.getDate());
+    const refYear = refDate.getFullYear();
+    const refMonth = refDate.getMonth();
+    const refDay = refDate.getDate();
+    const refDayOfWeek = refDate.getDay();
+    console.debug('today', refYear, refMonth, refDay, refDayOfWeek);
 
-      // 목록에서 클릭한 일기에 액티브 클래스 추가
-      diaryList.childNodes.forEach((c) => {
-        c.classList.remove('active');
-      });
-      clickedLabel.classList.add('active');
+    const firstDateOfMonth = new Date(refYear, refMonth, 1);
+    const firstDayOfMonth = firstDateOfMonth.getDay();
+    console.debug('first of month', firstDateOfMonth.toLocaleDateString(), firstDayOfMonth);
 
-      // 일기를 화면에 로딩
-      diaryTitle.value = diary.title;
-      diaryDate.value = diary.date;
-      diaryDay.innerHTML = getDay();
-      document.querySelector('[name="diaryWeather"][value="' + diary.weather + '"]').click();
-      diaryEditor.setHTML(diary.content, false);
+    const firstDateOfCalendar = new Date(refYear, refMonth, 1 - firstDayOfMonth);
+    console.debug('first date of calendar', firstDateOfCalendar.toLocaleDateString());
 
-      // 제목, 본문 쓰기 모드 설정
-      diaryTitle.removeAttribute('readonly');
-      diaryEditor.show();
-    });
+    const lastDateOfMonth = new Date(refYear, refMonth + 1, 0);
+    const lastDayOfMonth = lastDateOfMonth.getDay();
+    console.debug('last date of month', lastDateOfMonth.toLocaleDateString(), lastDayOfMonth);
 
-    if (!diary.new) {
-      const weatherElement = document.createElement('i');
-      weatherElement.setAttribute('class', 'fa fa-' + weatherMap[diary.weather]);
-      labelElement.appendChild(weatherElement);
+    const lastDateOfCalendar = new Date(refYear, refMonth + 1, 6 - lastDayOfMonth);
+    console.debug('last date of calendar', lastDateOfCalendar.toLocaleDateString());
+
+    const diffDay = Math.abs((lastDateOfCalendar - firstDateOfCalendar) / (1000 * 60 * 60 * 24)) + 1;
+    console.debug('diff day', diffDay);
+
+    const dates = [];
+    for (let i = 0; i < diffDay; i++) {
+      const date = new Date(refYear, refMonth, 1 - firstDayOfMonth + i);
+      console.debug(date.toLocaleDateString());
+
+      dates.push(date);
     }
 
-    diaryList.prepend(labelElement);
+    html += `
+      <div class="month">
+        <h5 class="month-name">${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(refDate)}</h5>
+        <div class="dates">${dates
+          .map((date) => {
+            const year = date.getFullYear();
+            const month = date.getMonth();
+            const day = date.getDate();
+
+            const yyyymmdd = `${year}-${month + 1}-${day < 10 ? '0' + day : day}`;
+            const isSameMonth = refYear === year && refMonth === month;
+            const isToday = todayYear === year && todayMonth === month && todayDay === day;
+
+            return `<div class="date ${isSameMonth ? 'ref' : 'side'} ${isToday ? 'today' : ''}"
+                title="${date.toLocaleDateString()}"
+                id="d-${isSameMonth ? yyyymmdd : ''}">${day}</div>`;
+          })
+          .join('')}
+        </div>
+      </div>`;
   }
 
-  const today = new Date().toLocaleDateString().replace(/. /g, '-').substring(0, 10);
+  document.querySelector('.calendar').innerHTML = html;
+}
 
-  diaryList.replaceChildren();
+/**
+ * 달력 클릭 이벤트
+ */
+function addCalendarEventListener() {
+  document.querySelector('.calendar').addEventListener('click', (e) => {
+    console.log('click Calendar date');
 
-  fetch('/diary')
-    .then((response) => response.json())
-    .then((list) => {
-      list.forEach((diary) => appendDiary(diary));
-      appendDiary({
+    // 클릭 이벤트에서 label node 찾기
+    let clickedDate;
+    if (e.target.nodeName === 'DIV') {
+      clickedDate = e.target;
+    } else if (e.target.nodeName === 'I') {
+      clickedDate = e.target.parentNode;
+    } else {
+      throw Error('not found label');
+    }
+
+    // ref 날이 아니면, path
+    if (!clickedDate.classList.contains('ref')) {
+      return;
+    }
+
+    // 중복 클릭 방지
+    if (clickedDate.classList.contains('active')) {
+      return;
+    }
+
+    // 목록에서 클릭한 일기에 액티브 클래스 추가
+    document.querySelectorAll('.date').forEach((date) => {
+      date.classList.remove('active');
+    });
+    clickedDate.classList.add('active');
+
+    const date = clickedDate.id.substring(2);
+
+    if (clickedDate.classList.contains('written')) {
+      fetch('/diary/date/' + date)
+        .then((response) => response.json())
+        .then((diary) => loadDiary(diary));
+    } else {
+      loadDiary({
         title: '',
-        date: today,
+        date: date,
         weather: 'sunny',
         content: '',
-        new: true,
+      });
+    }
+  });
+}
+
+/**
+ * 일기쓴 날짜 로딩
+ */
+function markDiaryDates() {
+  fetch('/diary/dates')
+    .then((response) => response.json())
+    .then((list) => {
+      list.forEach((date) => {
+        const dateElement = document.querySelector('#d-' + date);
+        if (dateElement !== null) {
+          const markerElement = document.createElement('i');
+          markerElement.setAttribute('class', 'fa fa-star');
+
+          dateElement.classList.add('written');
+          dateElement.appendChild(markerElement);
+        }
       });
     });
+}
+
+/**
+ * 일기를 화면에 로딩
+ * @param {*} diary
+ */
+function loadDiary(diary) {
+  console.log('loadDiary', diary);
+
+  diaryTitle.value = diary.title;
+  diaryDate.value = diary.date;
+  diaryDay.innerHTML = getDay(diary.date);
+  document.querySelector('[name="diaryWeather"][value="' + diary.weather + '"]').click();
+  diaryEditor.setHTML(diary.content, false);
+
+  // 제목, 본문 쓰기 모드 설정
+  diaryTitle.removeAttribute('readonly');
+  diaryEditor.show();
 }
 
 /**
@@ -148,6 +229,12 @@ function saveDiary() {
 
   restCall('/diary', { method: 'POST', data: diary }, (diary) => {
     console.log('성공:', diary);
-    loadDiaryList();
+    markDiaryDates();
   });
+}
+
+// ======== utilities ========
+
+function getDay(diaryDate) {
+  return new Intl.DateTimeFormat('ko-KR', { weekday: 'short' }).format(new Date(diaryDate));
 }

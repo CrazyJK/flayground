cfb5c35c09e9397b565e46875fa42f4e46ee2cf9
jk kamoru <Crazy.4.JK@gmail.com>
2023-04-16 01:19:19 +0900

    add TODO

--- www/src/components/FlayCover.js
+++ www/src/components/FlayCover.js
@@ -4,18 +4,15 @@
 export default class FlayCover extends HTMLElement {
   constructor() {
     super();
-
     this.attachShadow({ mode: 'open' }); // 'this.shadowRoot'을 설정하고 반환합니다
-
-    this.flay = null;
     this.wrapper = document.createElement('div');
     this.wrapper.classList.add('cover');
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', './css/components.css');
+    this.shadowRoot.append(link, this.wrapper); // 생성된 요소들을 shadow DOM에 부착합니다
 
-    const style = document.createElement('link');
-    style.setAttribute('rel', 'stylesheet');
-    style.setAttribute('href', './css/components.css');
-
-    this.shadowRoot.append(style, this.wrapper); // 생성된 요소들을 shadow DOM에 부착합니다
+    this.flay = null;
   }
 
   /**
@@ -24,14 +21,15 @@ export default class FlayCover extends HTMLElement {
    */
   set(flay) {
     this.flay = flay;
-    this.wrapper.classList.toggle('archive', this.flay.archive);
     this.wrapper.setAttribute('data-opus', flay.opus);
+    this.wrapper.classList.toggle('archive', this.flay.archive);
     if (this.parentElement) {
       this.wrapper.classList.toggle('small', this.parentElement.classList.contains('small'));
     }
-
     this.wrapper.style.backgroundImage = `url(/static/cover/${flay.opus})`;
   }
+  // TODO 대표색상 추출
+  // TODO 영상 스트리링
 }
 
 // Define the new element

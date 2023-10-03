db6015d00a435177800fc19e99ad0af219d36453
Crazy JK <Crazy.4.JK@gmail.com>
2023-10-04 00:50:32 +0900

    emit

--- www/src/popup.flay.js
+++ www/src/popup.flay.js
@@ -7,4 +7,21 @@ const urlParams = new URL(location.href).searchParams;
 const opus = urlParams.get('opus');
 
 document.title = opus;
-document.querySelector('body').appendChild(new FlayPage()).set(opus);
+const flayPage = document.querySelector('body').appendChild(new FlayPage());
+flayPage.set(opus);
+
+window.emitFlay = (flay) => {
+  if (flayPage.opus === flay.opus) flayPage.reload();
+};
+window.emitStudio = (studio) => {
+  if (flayPage.flay.studio === studio.name) flayPage.reload();
+};
+window.emitVideo = (video) => {
+  if (flayPage.opus === video.opus) flayPage.reload();
+};
+window.emitActress = (actress) => {
+  if (Array.from(flayPage.flay.actressList).filter((name) => name === actress.name).length > 0) flayPage.reload();
+};
+window.emitTag = (tag) => {
+  flayPage.reload();
+};

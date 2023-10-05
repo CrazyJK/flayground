52e2fee6f23a50a48957713503377e860c44b003
Crazy JK <Crazy.4.JK@gmail.com>
2023-10-05 21:00:55 +0900

    Nesting 사용으로 위치이동

--- www/webpack.config.cjs
+++ www/webpack.config.cjs
@@ -1,8 +1,8 @@
-const commonConfig = require('./webpack/webpack.common.cjs');
+const commonConfig = require('./webpack.common.cjs');
 const { merge } = require('webpack-merge');
 const argv = require('yargs').argv;
 
 module.exports = () => {
-  const envConfig = require(`./webpack/webpack.${argv.env}.cjs`);
+  const envConfig = require(`./webpack.${argv.env}.cjs`);
   return merge(commonConfig, envConfig);
 };

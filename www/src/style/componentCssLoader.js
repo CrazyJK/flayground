d3dcc16b9955b3eb03c6e33196dae6853c084574
Crazy JK <Crazy.4.JK@gmail.com>
2023-10-05 19:49:49 +0900

    rename

--- www/src/style/componentCssLoader.js
+++ www/src/style/componentCssLoader.js
@@ -1,10 +1,9 @@
-import CommonCSS from 'raw-loader!./components.css';
+import componentCSS from 'raw-loader!./components.css';
 
 export default function (wrapper) {
-  console.log(typeof wrapper, wrapper);
   if (!wrapper) {
     wrapper = document.querySelector('head');
   }
   const style = wrapper.appendChild(document.createElement('style'));
-  style.innerHTML = CommonCSS;
+  style.innerHTML = componentCSS;
 }

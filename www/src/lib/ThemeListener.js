069532b99f826a4a75e65f30b32ac4b6c499afe6
Crazy JK <Crazy.4.JK@gmail.com>
2023-10-08 18:42:19 +0900

    theme enhance

--- www/src/lib/ThemeListener.js
+++ www/src/lib/ThemeListener.js
@@ -1,34 +1,40 @@
 import FlayStorage from '../util/FlayStorage';
 
+const THEME_KEY = 'FlayNav.theme';
+const OS = 'os';
+const LIGHT = 'light';
+const DARK = 'dark';
+
+const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+let theme = FlayStorage.local.get(THEME_KEY);
+let isDark;
+
 /**
  * Storage theme change Listener
  */
 onstorage = (e) => {
-  console.log('onstorage', e.key, e.oldValue, e.newValue);
+  console.debug('onstorage', e.key, e.oldValue, e.newValue);
   if (e.key === THEME_KEY) {
-    changeTheme(e.newValue);
+    theme = e.newValue;
+    applyTheme();
   }
 };
 
-const getPrefersColorSchemeDarkQuery = () => {
-  if (!window.matchMedia) {
-    return null;
+darkMediaQuery.addEventListener('change', () => {
+  if (theme === OS) {
+    applyTheme();
   }
-  return window.matchMedia('(prefers-color-scheme: dark)');
+});
+
+const applyTheme = () => {
+  if (theme === OS) {
+    isDark = darkMediaQuery.matches;
+  } else {
+    isDark = theme === DARK;
+  }
+
+  document.documentElement.setAttribute('theme', isDark ? DARK : LIGHT);
 };
 
-const changeTheme = (theme) => {
-  if (theme === 'os') {
-    const query = getPrefersColorSchemeDarkQuery();
-    if (query === null) {
-      theme = 'light';
-    } else {
-      theme = query.matches ? 'dark' : 'light';
-    }
-  }
-  document.getElementsByTagName('html')[0].setAttribute('theme', theme);
-};
-
-const THEME_KEY = 'FlayNav.theme';
-
-changeTheme(FlayStorage.local.get(THEME_KEY));
+applyTheme();
--- www/src/nav/part/ThemeController.js
+++ www/src/nav/part/ThemeController.js
@@ -2,79 +2,89 @@ import SVG from '../../svg/svg.json';
 import FlayStorage from '../../util/FlayStorage';
 import { componentCss } from '../../util/componentCssLoader';
 
+const THEME_KEY = 'FlayNav.theme';
+const OS = 'os';
+const LIGHT = 'light';
+const DARK = 'dark';
+
 export default class ThemeController extends HTMLElement {
-  constructor(listener) {
+  theme;
+  isDark;
+
+  constructor() {
     super();
     this.attachShadow({ mode: 'open' }); // 'this.shadowRoot'을 설정하고 반환합니다
 
-    const STYLE = document.createElement('style');
-    STYLE.innerHTML = CSS;
+    const style = document.createElement('style');
+    style.innerHTML = CSS;
     const wrapper = document.createElement('div');
     wrapper.classList.add('theme-controller');
-    this.shadowRoot.append(STYLE, wrapper); // 생성된 요소들을 shadow DOM에 부착합니다
+    this.shadowRoot.append(style, wrapper); // 생성된 요소들을 shadow DOM에 부착합니다
 
-    const DIV = wrapper.appendChild(document.createElement('div'));
-    DIV.classList.add('theme-group');
-    ['os', 'light', 'dark'].forEach((theme) => makeThemeRadio(DIV, theme));
+    const themeGroup = wrapper.appendChild(document.createElement('div'));
+    themeGroup.classList.add('theme-group');
+    [OS, LIGHT, DARK].forEach((theme) => {
+      const radio = themeGroup.appendChild(document.createElement('input'));
+      radio.type = 'radio';
+      radio.id = 'theme' + theme;
+      radio.name = 'theme';
+      radio.value = theme;
+      radio.title = theme;
+
+      const label = themeGroup.appendChild(document.createElement('label'));
+      label.setAttribute('for', 'theme' + theme);
+      label.innerHTML = SVG.theme[theme];
+      label.addEventListener('click', () => {
+        FlayStorage.local.set(THEME_KEY, theme);
+        console.debug('set Storage Theme', theme);
+
+        this.theme = theme;
+        this.applyTheme();
+      });
+    });
+  }
+
+  connectedCallback() {
+    this.theme = FlayStorage.local.get(THEME_KEY, OS);
+    this.mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+
+    this.onThemeListener();
+    this.applyTheme();
+  }
+
+  onThemeListener() {
+    // storage 변경에 대한 리스너 등록
+    onstorage = (e) => {
+      console.debug('[onThemeListener] onstorage', e.key, e.oldValue, e.newValue);
+      if (e.key === THEME_KEY) {
+        this.theme = e.newValue;
+        this.applyTheme();
+      }
+    };
+    // os 테마 변경에 대한 리스너 등록
+    this.mediaQueryList.addEventListener('change', () => {
+      console.debug('[onThemeListener] mediaQuery', this.mediaQueryList);
+      if (this.theme === OS) {
+        this.applyTheme();
+      }
+    });
+  }
+
+  applyTheme() {
+    if (this.theme === OS) {
+      this.isDark = this.mediaQueryList.matches;
+    } else {
+      this.isDark = this.theme === DARK;
+    }
+    console.debug('[applyTheme] theme', this.theme, 'isDark', this.isDark);
+
+    this.shadowRoot.querySelector('input#theme' + this.theme).checked = true;
+    document.documentElement.setAttribute('theme', this.isDark ? DARK : LIGHT);
   }
 }
 
 // Define the new element
 customElements.define('theme-controller', ThemeController);
-
-const asisTheme = FlayStorage.local.get('FlayNav.theme', 'os');
-
-const makeThemeRadio = (parent, theme) => {
-  const radio = parent.appendChild(document.createElement('input'));
-  radio.type = 'radio';
-  radio.name = 'theme';
-  radio.id = 'theme' + theme;
-  radio.value = theme;
-  radio.title = theme;
-  radio.addEventListener('click', (e) => {
-    if (e.target.value !== 'os') {
-      changeTheme(e.target.value === 'dark');
-    } else {
-      changeTheme(getPrefersColorSchemeDarkQuery().matches, true);
-    }
-  });
-
-  const label = parent.appendChild(document.createElement('label'));
-  label.setAttribute('for', 'theme' + theme);
-  label.innerHTML = SVG.theme[theme];
-
-  if (theme === asisTheme) {
-    radio.click();
-  }
-};
-
-const changeTheme = (isDarkMode, isOS) => {
-  let mode = isDarkMode ? 'dark' : 'light';
-  document.documentElement.setAttribute('theme', mode);
-  if (isOS) {
-    FlayStorage.local.set('FlayNav.theme', 'os');
-  } else {
-    FlayStorage.local.set('FlayNav.theme', mode);
-  }
-};
-
-const getPrefersColorSchemeDarkQuery = () => {
-  if (!window.matchMedia) {
-    return null;
-  }
-  return window.matchMedia('(prefers-color-scheme: dark)');
-};
-
-const runOsThemeListener = (fn) => {
-  const query = getPrefersColorSchemeDarkQuery();
-  if (query === null) {
-    return;
-  }
-  fn(query.matches);
-  query.addEventListener('change', (event) => fn(event.matches));
-};
-
-runOsThemeListener(changeTheme);
 
 const CSS = `
 ${componentCss}

bb1a306aecab83afebd7f53f2410b5cc30f9b72f
Crazy JK <Crazy.4.JK@gmail.com>
2023-10-08 23:42:31 +0900

    draggable

--- www/src/page.develop.js
+++ www/src/page.develop.js
@@ -26,25 +26,70 @@ async function appendDevelop() {
   const opus = getRandomOpus();
   const flayDevelop = new FlayDevelop(opus);
   await flayDevelop.ready();
+  flayDevelop.setMoveable();
 
-  const width = 800;
+  const article = document.querySelector('article');
+  const width = 600;
   const height = parseInt((width * 269) / 400);
-  const top = getRandomInt(0, window.innerHeight - height);
-  const left = getRandomInt(0, window.innerWidth - width);
+
+  const left = getRandomInt(0, article.offsetWidth - width);
+  const top = getRandomInt(0, article.offsetHeight - height);
 
   flayDevelop.style.position = 'absolute';
   flayDevelop.style.width = width + 'px';
-  flayDevelop.style.top = top + 'px';
   flayDevelop.style.left = left + 'px';
+  flayDevelop.style.top = top + 'px';
 
-  document.querySelector('main').append(flayDevelop);
+  article.append(flayDevelop);
 }
 
 function removeDevelop() {
-  const nodeList = document.querySelectorAll('flay-develop');
+  const nodeList = document.querySelectorAll('article flay-develop');
   for (let i = 0; i < nodeList.length - 5; i++) {
     nodeList[i].remove();
   }
 }
 
 document.addEventListener('wheel', start);
+
+window.dragged;
+
+document.querySelectorAll('.dropzone').forEach((dropzone) => {
+  dropzone.addEventListener(
+    'dragover',
+    (event) => {
+      // prevent default to allow drop
+      event.preventDefault();
+    },
+    false
+  );
+
+  dropzone.addEventListener('dragenter', (e) => {
+    console.log(e.type, e.target);
+    // highlight potential drop target when the draggable element enters it
+    if (e.target.classList.contains('dropzone')) {
+      e.target.classList.add('dragover');
+    }
+  });
+
+  dropzone.addEventListener('dragleave', (e) => {
+    console.log(e.type, e.target);
+    // reset background of potential drop target when the draggable element leaves it
+    if (e.target.classList.contains('dropzone')) {
+      e.target.classList.remove('dragover');
+    }
+  });
+
+  dropzone.addEventListener('drop', (e) => {
+    console.log(e.type, e);
+    // prevent default action (open as link for some elements)
+    e.preventDefault();
+    // move dragged element to the selected drop target
+    if (e.target.classList.contains('dropzone')) {
+      e.target.classList.remove('dragover');
+      if (window.dragged) {
+        e.target.appendChild(window.dragged);
+      }
+    }
+  });
+});

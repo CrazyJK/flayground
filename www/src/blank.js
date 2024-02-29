64e049d29bf828da082c83bc740275b3fc8dfac3
Crazy JK <Crazy.4.JK@gmail.com>
2024-02-29 22:55:32 +0900

    blank page

--- www/src/blank.js
+++ www/src/blank.js
@@ -27,23 +27,22 @@ class Page {
     this.coverURL = URL.createObjectURL(await fetch(`/static/cover/${flay.opus}`).then((res) => res.blob()));
 
     this.BODY.innerHTML = `
-      <img class="cover" src="${this.coverURL}">
-      <div class="studio">${flay.studio}</div>
-      <div class="opus">${flay.opus}</div>
-      <div class="title">${flay.title}</div>
-      <div class="actress">${flay.actressList.join(', ')}</div>
-      <div class="ralease">${flay.release}</div>
-      <div class="comment">${flay.video.comment}</div>
-      <div class="last-access">${dateFormat(flay.video.lastAccess, 'yyyy-mm-dd')}</div>
-      <div class="last-modified">${dateFormat(flay.video.lastModified, 'yyyy-mm-dd')}</div>
-      <div class="last-play">${dateFormat(flay.video.lastPlay, 'yyyy-mm-dd')}</div>
-      <div class="play">${toBlank(flay.video.play)}</div>
-      <div class="shot">${toBlank(flay.video.likes?.length)}</div>
-      <div class="rank">${toBlank(flay.video.rank)}</div>
-      <div class="tags">${toBlank(flay.video.tags?.map((t) => t.name).join(', '))}</div>
-      <div class="jp-title">${toBlank(flay.video.title)}</div>
-      <div class="jp-desc">${toBlank(flay.video.desc)}</div>
-    `;
+      <img class="cover"    src="${this.coverURL}">
+      <div class="studio"       >${flay.studio}                                            </div>
+      <div class="opus"         >${flay.opus}                                              </div>
+      <div class="title"        >${flay.title}                                             </div>
+      <div class="actress"      >${flay.actressList.join(', ')}                            </div>
+      <div class="ralease"      >${flay.release}                                           </div>
+      <div class="comment"      >${toBlank(flay.video.comment)}                            </div>
+      <div class="last-access"  >${dateFormat(flay.video.lastAccess, 'yyyy-mm-dd')}        </div>
+      <div class="last-modified">${dateFormat(flay.video.lastModified, 'yyyy-mm-dd')}      </div>
+      <div class="last-play"    >${dateFormat(flay.video.lastPlay, 'yyyy-mm-dd')}          </div>
+      <div class="play"         >${toBlank(flay.video.play)}                               </div>
+      <div class="shot"         >${toBlank(flay.video.likes?.length)}                      </div>
+      <div class="rank"         >${toBlank(flay.video.rank)}                               </div>
+      <div class="tags"         >${toBlank(flay.video.tags?.map((t) => t.name).join(', '))}</div>
+      <div class="jp-title"     >${toBlank(flay.video.title)}                              </div>
+      <div class="jp-desc"      >${toBlank(flay.video.desc)}                               </div>`;
 
     console.log(flay);
   }

8f6e34587cd5ed0035d2f9a89774edaf63042ba9
Crazy JK <Crazy.4.JK@gmail.com>
2024-04-12 21:44:57 +0900

    enhance TabUI

--- www/src/lib/TabUI.js
+++ www/src/lib/TabUI.js
@@ -1,51 +1,31 @@
+import './TabUI.scss';
+
 /**
  * 탭 UI 처리
  *
+ * ```html
  * <button role="tab" target="#studioActress" [active] ...>
  * <div role="tabpanel" id="studioActress" ...>
+ * ```
+ * @param {Element} root
  */
-
-import './TabUI.scss';
-
-const tablistNodeList = document.querySelectorAll('[role="tablist"]');
-console.debug('tablistNodeList', tablistNodeList);
-tablistNodeList.forEach((group) => {
-  //
-  const tabNodeList = group.querySelectorAll('[role="tab"]');
-  console.debug('tabNodeList', tabNodeList);
-  tabNodeList.forEach((tab) => {
+export const tabUI = (root) => {
+  root.querySelectorAll('[role="tablist"]').forEach((tablist) => {
     //
-    const target = tab.getAttribute('target');
-    const active = tab.hasAttribute('active');
-    const targetPanel = document.querySelector(target);
-    console.debug('tab', tab, targetPanel, active);
+    tablist.querySelectorAll('[role="tab"]').forEach((thisTab, i, tabs) => {
+      //
+      thisTab.addEventListener('click', () => {
+        //
+        tabs.forEach((tab) => {
+          //
+          const isEquals = thisTab === tab;
 
-    if (active) {
-      targetPanel.setAttribute('active', true);
-    } else {
-      targetPanel.removeAttribute('active');
-    }
-
-    tab.addEventListener('click', () => {
-      activateTab(tab, tabNodeList);
+          tab.toggleAttribute('active', isEquals);
+          root.querySelector(tab.getAttribute('target')).toggleAttribute('active', isEquals);
+        });
+      });
     });
-  });
-});
 
-function activateTab(checkedTab, tabNodeList) {
-  console.debug('activateTab args', checkedTab, tabNodeList);
-  tabNodeList.forEach((tab) => {
-    //
-    const target = tab.getAttribute('target');
-    const targetPanel = document.querySelector(target);
-    console.debug('activateTab', checkedTab, tab);
-
-    if (tab === checkedTab) {
-      tab.setAttribute('active', true);
-      targetPanel.setAttribute('active', true);
-    } else {
-      tab.removeAttribute('active');
-      targetPanel.removeAttribute('active');
-    }
+    tablist.querySelector('[role="tab"][active]')?.click();
   });
-}
+};

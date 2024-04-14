a4feb591130633ff9804657fb9be511af9d50af6
Crazy JK <Crazy.4.JK@gmail.com>
2024-04-14 17:54:06 +0900

    show info

--- www/src/popup.cover.js
+++ www/src/popup.cover.js
@@ -4,5 +4,15 @@ import './popup.cover.scss';
 const urlParams = new URL(location.href).searchParams;
 const opus = urlParams.get('opus');
 
-document.title = opus;
-document.querySelector('body').style.backgroundImage = `url(/static/cover/${opus})`;
+(async () => {
+  const res = await fetch(`/static/cover/${opus}/withData`);
+  const data = res.headers.get('Data');
+  const dataDecoded = decodeURIComponent(data.replace(/\+/g, ' '));
+  const flay = JSON.parse(dataDecoded);
+  const coverBlob = await res.blob();
+  const coverURL = URL.createObjectURL(coverBlob);
+
+  document.title = `[${flay.studio}][${flay.opus}][${flay.title}][${flay.actressList.join(',')}][${flay.release}]`;
+
+  document.querySelector('body').style.backgroundImage = `url(${coverURL})`;
+})();

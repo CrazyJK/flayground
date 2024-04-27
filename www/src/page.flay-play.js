fe79913bfbb3e32dc1bfd0d39327c08a92e4e31e
Crazy JK <Crazy.4.JK@gmail.com>
2024-04-27 22:42:02 +0900

    FlayVideoPlayer 적용

--- www/src/page.flay-play.js
+++ www/src/page.flay-play.js
@@ -6,13 +6,15 @@ import './flay/part/FlayStudio';
 import './flay/part/FlayTag';
 import './flay/part/FlayTitle';
 
+import './flay/part/FlayVideoPlayer';
+
 import './init/Page';
 import { FlayProvider } from './lib/FlayProvider';
 import './page.flay-play.scss';
 import { getRandomInt } from './util/randomNumber';
 
 class Page extends FlayProvider {
-  video;
+  videoPlayer;
   progressTimer;
 
   /** 최소 플레이 초 */
@@ -23,48 +25,32 @@ class Page extends FlayProvider {
   constructor() {
     super();
 
-    this.video = document.querySelector('video');
-    this.video.toggleAttribute('controls', true);
-    this.video.volume = 0.1;
+    this.videoPlayer = document.querySelector('flay-video-player');
+    this.videoPlayer.setAttribute('controls', true);
+    this.videoPlayer.setAttribute('volume', 0.1);
   }
 
   async play() {
     try {
       const { opus, flay, actress } = await this.random();
-      // const { opus, flay, actress } = await this.get('SNIS-233');
-
       console.log('play', opus);
 
-      this.#showFlayInfo(opus, flay, actress);
+      document.querySelector('main').style.backgroundImage = `url(/static/cover/${opus})`;
 
-      this.video.poster = `/static/cover/${opus}`;
-      this.video.src = `/stream/flay/movie/${opus}/0`;
+      this.videoPlayer.set(opus, flay, actress);
+      const duration = await this.videoPlayer.play();
+      const seekTime = getRandomInt(1, duration - this.MaxPlayTime);
+      await this.videoPlayer.play(seekTime);
 
-      await this.video.play();
-      this.video.currentTime = getRandomInt(1, this.video.duration - this.MaxPlayTime);
-      console.log(`played from ${this.video.currentTime} seconds`);
-
-      this.#setPlayTimeAndNext();
+      this.#setPlayTimeAndNext(seekTime, duration);
     } catch (error) {
       console.error(error);
       this.play();
     }
   }
 
-  #showFlayInfo(opus, flay, actress) {
-    document.querySelector('main').style.backgroundImage = `url(/static/cover/${opus})`;
-    document.querySelector('flay-studio').set(flay);
-    document.querySelector('flay-opus').set(flay);
-    document.querySelector('flay-title').set(flay);
-    document.querySelector('flay-actress').set(flay, actress);
-    document.querySelector('flay-release').set(flay);
-    document.querySelector('flay-rank').set(flay);
-    document.querySelector('flay-tag').set(flay);
-    document.querySelector('flay-tag').setCard();
-  }
-
-  #setPlayTimeAndNext() {
-    const leftTime = this.video.duration - this.video.currentTime; // 남은 시간 초
+  #setPlayTimeAndNext(seekTime, duration) {
+    const leftTime = duration - seekTime; // 남은 시간 초
     const playTime = getRandomInt(this.MinPlayTime, Math.min(leftTime, this.MaxPlayTime));
 
     const progressBar = document.querySelector('.progress-bar');

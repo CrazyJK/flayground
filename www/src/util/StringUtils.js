5eb76914027218d92ca37c7ef24550ae1806e8e1
Crazy JK <Crazy.4.JK@gmail.com>
2024-06-03 20:28:29 +0900

    string이 아닐때 예외처리

--- www/src/util/StringUtils.js
+++ www/src/util/StringUtils.js
@@ -1,10 +1,11 @@
 export default class StringUtils {
   /**
    * 값이 없거나, 빈값, 공백뿐인지
-   * @param {string?} text
+   * @param {string} text
    * @returns
    */
   static isBlank(text) {
+    if (typeof text !== 'string') return false;
     return typeof text === 'undefined' || text === null || text.trim() === '';
   }
 

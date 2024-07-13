c5b00c7a94e6eb25bf7ebe223effa36b26acfb72
Crazy JK <Crazy.4.JK@gmail.com>
2024-07-14 00:07:43 +0900

    FlayArticle

--- www/src/page.archive.js
+++ www/src/page.archive.js
@@ -1,14 +1,10 @@
-import FlayPagination from './flay/page/FlayPagination';
 import './init/Page';
 import './page.archive.scss';
-import DateUtils from './util/DateUtils';
-import StringUtils from './util/StringUtils';
+
+import FlayArticle from './flay/FlayArticle';
+import FlayPagination from './flay/page/FlayPagination';
 
 class Page {
-  coverURL;
-
-  constructor() {}
-
   async start() {
     const list = await fetch('/archive').then((res) => res.json());
     list.sort((a1, a2) => a1.release.localeCompare(a2.release));
@@ -18,35 +14,15 @@ class Page {
       return map;
     }, new Map());
 
-    const flayPagination = document.querySelector('body > footer').appendChild(new FlayPagination());
-    flayPagination.addEventListener('change', async (e) => {
+    const flayArticle = document.querySelector('main').appendChild(new FlayArticle());
+    const flayPagination = document.querySelector('footer').appendChild(new FlayPagination());
+
+    flayPagination.addEventListener('change', (e) => {
       const opus = e.target.opus;
-      await this.#show(archiveMap.get(opus));
+      const flay = archiveMap.get(opus);
+      flayArticle.set(flay);
     });
     flayPagination.set(Array.from(archiveMap.keys()));
-  }
-
-  async #show(flay) {
-    URL.revokeObjectURL(this.coverURL);
-    this.coverURL = URL.createObjectURL(await fetch(`/static/cover/${flay.opus}`).then((res) => res.blob()));
-
-    document.querySelector('main').innerHTML = `
-      <div class="cover" style="background-image: url(${this.coverURL})">                              </div>
-      <div class="studio"       >${flay.studio}                                                        </div>
-      <div class="opus"         >${flay.opus}                                                          </div>
-      <div class="title"        >${flay.title}                                                         </div>
-      <div class="actress"      >${flay.actressList.join(', ')}                                        </div>
-      <div class="ralease"      >${flay.release}                                                       </div>
-      <div class="comment"      >${StringUtils.toBlank(flay.video.comment)}                            </div>
-      <div class="last-access"  >${DateUtils.format(flay.video.lastAccess, 'yyyy-MM-dd')}              </div>
-      <div class="last-modified">${DateUtils.format(flay.video.lastModified, 'yyyy-MM-dd')}            </div>
-      <div class="last-play"    >${DateUtils.format(flay.video.lastPlay, 'yyyy-MM-dd')}                </div>
-      <div class="play"         >${StringUtils.toBlank(flay.video.play)}                               </div>
-      <div class="shot"         >${StringUtils.toBlank(flay.video.likes?.length)}                      </div>
-      <div class="rank"         >${StringUtils.toBlank(flay.video.rank)}                               </div>
-      <div class="tags"         >${StringUtils.toBlank(flay.video.tags?.map((t) => t.name).join(', '))}</div>
-      <div class="jp-title"     >${StringUtils.toBlank(flay.video.title)}                              </div>
-      <div class="jp-desc"      >${StringUtils.toBlank(flay.video.desc)}                               </div>`;
   }
 }
 
--- www/src/page.flay-one.js
+++ www/src/page.flay-one.js
@@ -1,80 +1,34 @@
 import './init/Page';
 import './page.flay-one.scss';
 
+import FlayArticle from './flay/FlayArticle';
 import FlayCondition from './flay/page/FlayCondition';
 import { OpusProvider } from './lib/OpusProvider';
-import DateUtils from './util/DateUtils';
-import StringUtils from './util/StringUtils';
 
 class Page extends OpusProvider {
-  opus;
-  coverURL;
-  flayContainer;
-
   constructor() {
     super();
-
-    this.flayContainer = document.querySelector('body > main > article');
-  }
-
-  #showCover() {
-    URL.revokeObjectURL(this.coverURL);
-    document.startViewTransition(async () => {
-      this.opus = await this.getRandomOpus();
-
-      const res = await fetch(`/static/cover/${this.opus}/withData`);
-      const data = res.headers.get('Data');
-      const dataDecoded = decodeURIComponent(data.replace(/\+/g, ' '));
-      const flay = JSON.parse(dataDecoded);
-      const coverBlob = await res.blob();
-
-      this.coverURL = URL.createObjectURL(coverBlob);
-
-      this.flayContainer.innerHTML = `
-      <img class="cover"    src="${this.coverURL}">
-      <div class="studio"       >${flay.studio}                                                        </div>
-      <div class="opus"         >${flay.opus}                                                          </div>
-      <div class="title"        >${flay.title}                                                         </div>
-      <div class="actress"      >${flay.actressList.join(', ')}                                        </div>
-      <div class="ralease"      >${flay.release}                                                       </div>
-      <div class="comment"      >${StringUtils.toBlank(flay.video.comment)}                            </div>
-      <div class="last-access"  >${DateUtils.format(flay.video.lastAccess, 'yyyy-MM-dd')}              </div>
-      <div class="last-modified">${DateUtils.format(flay.video.lastModified, 'yyyy-MM-dd')}            </div>
-      <div class="last-play"    >${DateUtils.format(flay.video.lastPlay, 'yyyy-MM-dd')}                </div>
-      <div class="play"         >${StringUtils.toBlank(flay.video.play)}                               </div>
-      <div class="shot"         >${StringUtils.toBlank(flay.video.likes?.length)}                      </div>
-      <div class="rank"         >${StringUtils.toBlank(flay.video.rank)}                               </div>
-      <div class="tags"         >${StringUtils.toBlank(flay.video.tags?.map((t) => t.name).join(', '))}</div>
-      <div class="jp-title"     >${StringUtils.toBlank(flay.video.title)}                              </div>
-      <div class="jp-desc"      >${StringUtils.toBlank(flay.video.desc)}                               </div>`;
-    });
   }
 
   async start() {
-    const flayCondition = document.querySelector('body > main > header').appendChild(new FlayCondition());
-    flayCondition.addEventListener('change', async () => {
-      this.setOpusList(flayCondition.opusList);
+    this.flayCondition = document.querySelector('header').appendChild(new FlayCondition());
+    this.flayArticle = document.querySelector('article').appendChild(new FlayArticle());
 
-      this.#showCover();
-    });
-
-    this.flayContainer.addEventListener('click', (e) => {
-      if (e.target.tagName === 'IMG') {
-        // window.open(`/static/cover/${this.opus}`, `cover.${this.opus}`, 'width=800px,height=538px');
-        window.open(`popup.cover.html?opus=${this.opus}`, `cover.${this.opus}`, 'width=800px,height=538px');
-      } else {
-        switch (e.target.className) {
-          case 'title':
-            window.open(`popup.flay.html?opus=${this.opus}`, `popup.${this.opus}`, 'width=800px,height=1280px');
-            break;
-          default:
-            break;
-        }
-      }
+    this.flayCondition.addEventListener('change', async () => {
+      this.setOpusList(this.flayCondition.opusList);
+      this.#show();
     });
 
     window.addEventListener('wheel', (e) => {
-      if (e.deltaY > 0) this.#showCover();
+      if (e.deltaY > 0) this.#show();
+    });
+  }
+
+  #show() {
+    document.startViewTransition(async () => {
+      const opus = await this.getRandomOpus();
+      const flay = await fetch(`/flay/${opus}`).then((res) => res.json());
+      this.flayArticle.set(flay);
     });
   }
 }

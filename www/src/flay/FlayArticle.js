2fd127f05d4c25a6c914b8a46a7d3c0af80f113a
Crazy JK <Crazy.4.JK@gmail.com>
2024-09-28 22:38:01 +0900

    스타일 수정

--- www/src/flay/FlayArticle.js
+++ www/src/flay/FlayArticle.js
@@ -16,28 +16,28 @@ export default class FlayArticle extends HTMLDivElement {
     this.innerHTML = `
       <div class="cover"></div>
       <dl>
-        <dt><span class="studio"        ></span></dt>
-        <dt><span class="opus"          ></span></dt>
-        <dt><span class="title"         ></span></dt>
-        <dt class="actress-wrap"></dt>
-        <dt><span class="release"       ></span></dt>
-        <dt><span class="tags"          ></span></dt>
-        <dd><span class="rank"          ></span> <small>rank</small></dd>
-        <dd><span class="play"          ></span> <small>play</small></dd>
-        <dd><span class="shot"          ></span> <small>shot</small></dd>
-        <dd><span class="comment"       ></span></dd>
-        <dd><span class="jp-title"      ></span></dd>
-        <dd><span class="jp-desc"       ></span></dd>
-        <dd><span class="last-access"   ></span> <small>accessed</small></dd>
-        <dd><span class="last-modified" ></span> <small>modified</small></dd>
-        <dd><span class="last-play"     ></span> <small>played  </small></dd>
+        <dt class="studio"       ></dt>
+        <dt class="opus"         ></dt>
+        <dt class="title"        ></dt>
+        <dt class="actress"      ></dt>
+        <dt class="release"      ></dt>
+        <dt class="tags"         ></dt>
+        <dd class="rank"         ></dd>
+        <dd class="play"         ></dd>
+        <dd class="shot"         ></dd>
+        <dd class="comment"      ></dd>
+        <dd class="jp-title"     ></dd>
+        <dd class="jp-desc"      ></dd>
+        <dd class="last-access"  ></dd>
+        <dd class="last-modified"></dd>
+        <dd class="last-play"    ></dd>
       </dl>
     `;
 
     this.querySelector('.cover').addEventListener('click', () => popupCover(this.opus));
     this.querySelector('.opus').addEventListener('click', () => popupFlayInfo(this.opus));
     this.querySelector('.title').addEventListener('click', () => popupFlay(this.opus));
-    this.querySelector('.actress-wrap').addEventListener('click', (e) => popupActress(e.target.textContent));
+    this.querySelector('.actress').addEventListener('click', (e) => popupActress(e.target.textContent));
   }
 
   set(flay, coverURL) {
@@ -48,21 +48,21 @@ export default class FlayArticle extends HTMLDivElement {
     this.querySelector('.studio').innerHTML = flay.studio;
     this.querySelector('.opus').innerHTML = flay.opus;
     this.querySelector('.title').innerHTML = flay.title;
-    this.querySelector('.actress-wrap').innerHTML = flay.actressList.map((name) => `<span>${name}</span>`).join(', ');
+    this.querySelector('.actress').innerHTML = flay.actressList.map((name) => `<span>${name}</span>`).join(', ');
     this.querySelector('.release').innerHTML = flay.release;
-
-    this.querySelector('.rank').innerHTML = StringUtils.toBlank(flay.video.rank);
-    this.querySelector('.play').innerHTML = StringUtils.toBlank(flay.video.play);
-    this.querySelector('.shot').innerHTML = StringUtils.toBlank(flay.video.likes?.length);
     this.querySelector('.tags').innerHTML = StringUtils.toBlank(flay.video.tags?.map((t) => t.name).join(', '));
+
+    this.querySelector('.rank').innerHTML = StringUtils.toBlank(flay.video.rank) + '<small>rank</small>';
+    this.querySelector('.play').innerHTML = StringUtils.toBlank(flay.video.play) + '<small>play</small>';
+    this.querySelector('.shot').innerHTML = StringUtils.toBlank(flay.video.likes?.length) + '<small>shot</small>';
     this.querySelector('.comment').innerHTML = StringUtils.toBlank(flay.video.comment);
 
     this.querySelector('.jp-title').innerHTML = StringUtils.toBlank(flay.video.title);
     this.querySelector('.jp-desc').innerHTML = StringUtils.toBlank(flay.video.desc);
 
-    this.querySelector('.last-access').innerHTML = flay.video.lastAccess > 0 ? DateUtils.format(flay.video.lastAccess, 'yy.MM.dd') : '';
-    this.querySelector('.last-modified').innerHTML = flay.video.lastModified > 0 ? DateUtils.format(flay.video.lastModified, 'yy.MM.dd') : '';
-    this.querySelector('.last-play').innerHTML = flay.video.lastPlay > 0 ? DateUtils.format(flay.video.lastPlay, 'yy.MM.dd') : '';
+    this.querySelector('.last-access').innerHTML = flay.video.lastAccess > 0 ? DateUtils.format(flay.video.lastAccess, 'yy.MM.dd') + '<small>accessed</small>' : '';
+    this.querySelector('.last-modified').innerHTML = flay.video.lastModified > 0 ? DateUtils.format(flay.video.lastModified, 'yy.MM.dd') + '<small>modified</small>' : '';
+    this.querySelector('.last-play').innerHTML = flay.video.lastPlay > 0 ? DateUtils.format(flay.video.lastPlay, 'yy.MM.dd') + '<small>played</small>' : '';
   }
 }
 

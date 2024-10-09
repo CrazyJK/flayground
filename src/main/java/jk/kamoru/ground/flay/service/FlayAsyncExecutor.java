6eefba6e57df9547b96f36754899238cfa8b409a
Crazy JK <Crazy.4.JK@gmail.com>
2024-10-09 20:51:04 +0900

    process ending info

--- src/main/java/jk/kamoru/ground/flay/service/FlayAsyncExecutor.java
+++ src/main/java/jk/kamoru/ground/flay/service/FlayAsyncExecutor.java
@@ -1,21 +1,25 @@
 package jk.kamoru.ground.flay.service;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
+import java.lang.ProcessHandle.Info;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import jk.kamoru.ground.base.web.sse.LogAndSse;
 import jk.kamoru.ground.flay.FlayException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class FlayAsyncExecutor {
+public class FlayAsyncExecutor extends LogAndSse {
 
   @Async
   public void exec(File app, List<File> arguments) {
@@ -42,14 +46,33 @@ public class FlayAsyncExecutor {
     execute(commands);
   }
 
-  private void execute(List<String> commands) {
+  private void execute(List<String> command) {
+    log.info("execute {}", command);
     try {
-      Process process = new ProcessBuilder(commands).redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).start();
-      log.info("exec {}", commands);
-      log.debug("process {}", process.info());
-    } catch (IOException e) {
+      Process process = new ProcessBuilder(command).redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).start();
+
+      Future<Boolean> identical = process.onExit().thenApply(p -> p.exitValue() == 0);
+      if (identical.get()) {
+        log.info("process is exited");
+
+        final Info info = process.info();
+        log.debug("process info {}", info);
+
+        final Instant start = info.startInstant().get();
+        final Instant end = Instant.now();
+        final Duration runDuration = Duration.between(start, end);
+        log.debug("process run time {}s", runDuration.getSeconds());
+
+        final Duration cpuDuration = info.totalCpuDuration().get();
+        log.debug("process CPU time {}s", cpuDuration.getSeconds());
+
+        noticeLogger("process run time " + runDuration.getSeconds() + "s");
+      }
+    } catch (Exception e) {
       log.error("exec error", e);
       throw new FlayException("exec error", e);
+    } finally {
+      log.info("execute end");
     }
   }
 

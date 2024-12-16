cbf5a88266de145a99c3783ecc8a5c385892f9b3
Crazy JK <Crazy.4.JK@gmail.com>
2024-12-16 19:25:54 +0900

    기울기각도

--- www/src/flay/panel/PopoutCover.js
+++ www/src/flay/panel/PopoutCover.js
@@ -7,11 +7,13 @@ export default class 커버튀어나오기 extends OpusProvider {
   #최소초;
   #최대초;
   #나오기완료;
-  #원래크기_키프레임;
-  #크기0_키프레임;
-  #크기110퍼_키프레임;
-  #애니메이션옵션;
 
+  /**
+   *
+   * @param {Element} 판때기
+   * @param {number} 최소초
+   * @param {number} 최대초
+   */
   constructor(판때기, 최소초 = 10, 최대초 = 30) {
     super();
 
@@ -19,23 +21,28 @@ export default class 커버튀어나오기 extends OpusProvider {
     this.#최소초 = 최소초;
     this.#최대초 = 최대초;
     this.#나오기완료 = true;
-    this.#원래크기_키프레임 = { transform: 'none' };
-    this.#크기0_키프레임 = { transform: 'scale(0)' };
-    this.#크기110퍼_키프레임 = { transform: 'scale(1.1)', offset: 0.7 };
-    this.#애니메이션옵션 = { duration: 500, iterations: 1 };
   }
 
-  async 계속나오기(이벤트) {
+  /**
+   *
+   * @param {Event} 이벤트
+   */
+  async 계속나오기(이벤트 = null) {
     await this.하나나오기(이벤트);
     setTimeout(async () => await this.계속나오기(), 1000 * getRandomInt(this.#최소초, this.#최대초));
   }
 
-  async 하나나오기(이벤트) {
+  /**
+   *
+   * @param {Event} 이벤트
+   * @returns
+   */
+  async 하나나오기(이벤트 = null) {
     if (!this.#나오기완료) return;
     this.#나오기완료 = false;
 
     this.#판때기.querySelectorAll('img').forEach((커버) => {
-      커버.animate([this.#원래크기_키프레임, this.#크기0_키프레임], this.#애니메이션옵션).finished.then(() => {
+      커버.animate([{ transform: 'none' }, { transform: 'scale(0)' }], { duration: 400, iterations: 1 }).finished.then(() => {
         커버.remove();
       });
     });
@@ -43,28 +50,38 @@ export default class 커버튀어나오기 extends OpusProvider {
     const 커버비율 = 269 / 400;
     const 여유공간 = 100;
     const { width: 판때기너비, height: 판때기높이 } = this.#판때기.getBoundingClientRect();
-    const 너비 = Math.min(1200, 판때기너비 / 2) + getRandomInt(0, 여유공간 * 2);
+    const 너비 = Math.min(1000, 판때기너비 / 2) + getRandomInt(0, 여유공간 * 2);
     const 높이 = 너비 * 커버비율;
     const 왼쪽 = 이벤트 ? 이벤트.x - 너비 / 2 : getRandomInt(여유공간, 판때기너비 - 너비 - 여유공간);
     const 위 = 이벤트 ? 이벤트.y - 높이 / 2 : getRandomInt(여유공간, 판때기높이 - 높이 - 여유공간);
-
+    const 기울기각도 = getRandomInt(-10, 10);
     const 품번 = await this.getRandomOpus();
     const 커버 = new Image();
     커버.style.cssText = `
       position: absolute;
-      left: ${왼쪽}px;
       top: ${위}px;
+      left: ${왼쪽}px;
       width: ${너비}px;
       height: auto;
       box-shadow: var(--box-shadow);
-      border-radius: 0.25rem;`;
+      border-radius: 0.25rem;
+      transform: rotate(${기울기각도}deg);
+      transition: 0.4s`;
     커버.src = `/static/cover/${품번}`;
     커버.onclick = () => popupFlay(품번);
-    커버.onload = () => {
+    커버.onload = async () => {
       this.#판때기.appendChild(커버);
-      커버.animate([this.#크기0_키프레임, this.#크기110퍼_키프레임, this.#원래크기_키프레임], this.#애니메이션옵션).finished.then(() => {
-        this.#나오기완료 = true;
-      });
+      await 커버.animate(
+        [
+          { offset: 0.0, transform: 'scale(0)' },
+          { offset: 0.5, transform: 'scale(1.1)' },
+          { offset: 0.7, transform: 'scale(1.0)' },
+          { offset: 0.8, transform: `rotate(${기울기각도 + 10}deg)` },
+          { offset: 1.0, transform: `rotate(${기울기각도}deg)` },
+        ],
+        { duration: 800, iterations: 1 }
+      ).finished;
+      this.#나오기완료 = true;
     };
   }
 }

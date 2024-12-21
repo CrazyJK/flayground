8bd6dfd486e69949c42e81808dd294fcc4015408
Crazy JK <Crazy.4.JK@gmail.com>
2024-12-21 12:35:30 +0900

    style

--- www/src/view/index.js
+++ www/src/view/index.js
@@ -5,12 +5,12 @@ class Page {
   constructor() {}
 
   async start() {
-    document.addEventListener(
+    const main = document.querySelector('body > main');
+    main.style.position = 'fixed';
+    main.style.inset = 0;
+    main.addEventListener(
       'click',
       (e) => {
-        const main = document.querySelector('body > main');
-        main.style.position = 'fixed';
-        main.style.inset = 0;
         import(/* webpackChunkName: "PopoutCover" */ '../flay/panel/PopoutCover').then((module) => new module.default(main).계속나오기(e));
       },
       { once: true }

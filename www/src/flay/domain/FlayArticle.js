10de5f8959d9f41d2278e789339e58e41d840da9
Crazy JK <Crazy.4.JK@gmail.com>
2025-01-07 15:33:27 +0900

    formated

--- www/src/flay/domain/FlayArticle.js
+++ www/src/flay/domain/FlayArticle.js
@@ -20,7 +20,7 @@ export default class FlayArticle extends HTMLDivElement {
         <dt class="studio"       ><span></span></dt>
         <dt class="opus"         ><span></span></dt>
         <dt class="title"        ><span></span></dt>
-        <dt class="actress"      ></dt>
+        <dt class="actress"      >             </dt>
         <dt class="release"      ><span></span></dt>
         <dt class="tags"         ><span></span></dt>
         <dd class="rank"         ><span></span></dd>
@@ -34,42 +34,42 @@ export default class FlayArticle extends HTMLDivElement {
         <dd class="last-play"    ><span></span></dd>
       </dl>
     `;
-
-    this.querySelector('.cover').addEventListener('click', () => {
-      import(/* webpackChunkName: "FlayVideoPlayer" */ '../panel/FlayVideoPlayer').then((module) => module.playInLayer(this.opus));
-    });
-    this.querySelector('.opus span').addEventListener('click', () => popupFlayInfo(this.opus));
-    this.querySelector('.title span').addEventListener('click', () => popupFlay(this.opus));
-    this.querySelector('.actress').addEventListener('click', (e) => {
-      if (e.target.tagName === 'SPAN') popupActress(e.target.textContent);
-    });
   }
 
   set(flay) {
-    this.opus = flay.opus;
-
-    FlayFetch.getCover(this.opus).then((url) => {
-      this.querySelector('.cover').style.backgroundImage = `url(${url})`;
-    });
-
-    this.querySelector('.studio span').innerHTML = flay.studio;
-    this.querySelector('.opus span').innerHTML = flay.opus;
-    this.querySelector('.title span').innerHTML = flay.title;
-    this.querySelector('.actress').innerHTML = flay.actressList.map((name) => `<span>${name}</span>`).join(', ');
-    this.querySelector('.release span').innerHTML = flay.release;
-    this.querySelector('.tags span').innerHTML = StringUtils.toBlank(flay.video.tags?.map((t) => t.name).join(', '));
-
-    this.querySelector('.rank span').innerHTML = StringUtils.toBlank(flay.video.rank) + '<small>rank</small>';
-    this.querySelector('.play span').innerHTML = StringUtils.toBlank(flay.video.play) + '<small>play</small>';
-    this.querySelector('.shot span').innerHTML = StringUtils.toBlank(flay.video.likes?.length) + '<small>shot</small>';
-    this.querySelector('.comment span').innerHTML = StringUtils.toBlank(flay.video.comment);
-
-    this.querySelector('.jp-title span').innerHTML = StringUtils.toBlank(flay.video.title);
-    this.querySelector('.jp-desc span').innerHTML = StringUtils.toBlank(flay.video.desc);
-
-    this.querySelector('.last-access span').innerHTML = flay.video.lastAccess > 0 ? DateUtils.format(flay.video.lastAccess, 'yy.MM.dd') + '<small>accessed</small>' : '';
+    this.querySelector('.studio        span').innerHTML = flay.studio;
+    this.querySelector('.opus          span').innerHTML = flay.opus;
+    this.querySelector('.title         span').innerHTML = flay.title;
+    this.querySelector('.actress').append(
+      ...flay.actressList.map((name) => {
+        const span = document.createElement('span');
+        span.innerHTML = name;
+        span.addEventListener('click', () => popupActress(name));
+        return span;
+      })
+    );
+    this.querySelector('.release       span').innerHTML = flay.release;
+    this.querySelector('.tags          span').innerHTML = StringUtils.toBlank(flay.video.tags?.map((tag) => tag.name).join(', '));
+    this.querySelector('.rank          span').innerHTML = StringUtils.toBlank(flay.video.rank) + '<small>rank</small>';
+    this.querySelector('.play          span').innerHTML = StringUtils.toBlank(flay.video.play) + '<small>play</small>';
+    this.querySelector('.shot          span').innerHTML = StringUtils.toBlank(flay.video.likes?.length) + '<small>shot</small>';
+    this.querySelector('.comment       span').innerHTML = StringUtils.toBlank(flay.video.comment);
+    this.querySelector('.jp-title      span').innerHTML = StringUtils.toBlank(flay.video.title);
+    this.querySelector('.jp-desc       span').innerHTML = StringUtils.toBlank(flay.video.desc);
+    this.querySelector('.last-access   span').innerHTML = flay.video.lastAccess > 0 ? DateUtils.format(flay.video.lastAccess, 'yy.MM.dd') + '<small>accessed</small>' : '';
     this.querySelector('.last-modified span').innerHTML = flay.video.lastModified > 0 ? DateUtils.format(flay.video.lastModified, 'yy.MM.dd') + '<small>modified</small>' : '';
-    this.querySelector('.last-play span').innerHTML = flay.video.lastPlay > 0 ? DateUtils.format(flay.video.lastPlay, 'yy.MM.dd') + '<small>played</small>' : '';
+    this.querySelector('.last-play     span').innerHTML = flay.video.lastPlay > 0 ? DateUtils.format(flay.video.lastPlay, 'yy.MM.dd') + '<small>played</small>' : '';
+
+    this.querySelector('.opus          span').addEventListener('click', () => popupFlayInfo(flay.opus));
+    this.querySelector('.title         span').addEventListener('click', () => popupFlay(flay.opus));
+
+    FlayFetch.getCover(flay.opus).then((url) => {
+      this.querySelector('.cover').style.backgroundImage = `url(${url})`;
+      if (!flay.archive)
+        this.querySelector('.cover').addEventListener('click', () => {
+          import(/* webpackChunkName: "FlayVideoPlayer" */ '../panel/FlayVideoPlayer').then((module) => module.playInLayer(flay.opus));
+        });
+    });
   }
 }
 

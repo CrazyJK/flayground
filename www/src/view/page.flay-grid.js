b7477ab9ca04aabb0c24ceb95f0e333b25795975
Crazy JK <Crazy.4.JK@gmail.com>
2025-01-09 23:51:00 +0900

    enhance

--- www/src/view/page.flay-grid.js
+++ www/src/view/page.flay-grid.js
@@ -9,54 +9,55 @@ import { getRandomInt } from '../lib/randomNumber';
 import { addResizeListener } from '../lib/windowAddEventListener';
 
 class Page {
-  opusList = [];
+  #opusList;
 
-  constructor() {}
-
-  async #showCover(opus) {
-    const flay = await FlayFetch.getFlay(opus);
-    const flayArticle = document.querySelector('main').appendChild(new FlayArticle({ mode: 'cover' }));
-    flayArticle.set(flay);
-
-    document.querySelector('#flayCount').innerHTML = this.opusList.length;
-
-    return window.innerHeight > flayArticle.getBoundingClientRect().top;
+  constructor() {
+    this.#opusList = [];
   }
 
-  async show() {
-    let visible = true;
-    do {
-      const opus = Math.random() > 0.5 ? this.opusList.shift() : this.opusList.splice(getRandomInt(0, this.opusList.length), 1)[0];
-      if (!opus) break;
+  start() {
+    document
+      .querySelector('body > header')
+      .appendChild(new FlayCondition())
+      .addEventListener('fetch', async (e) => {
+        this.#opusList = e.target.opusList;
 
-      visible = await this.#showCover(opus);
-    } while (visible);
-  }
-
-  async start() {
-    const flayCondition = document.querySelector('body > header').appendChild(new FlayCondition());
-    flayCondition.addEventListener('fetch', async () => {
-      this.opusList = flayCondition.opusList;
-
-      document.querySelector('#flayTotal').innerHTML = this.opusList.length;
-      document.querySelector('main').textContent = null;
-      await this.show();
-    });
+        document.querySelector('#flayTotal').innerHTML = this.#opusList.length;
+        document.querySelector('main').textContent = null;
+        await this.#append();
+      });
 
     document
       .querySelector('body > footer')
       .appendChild(new GridControl('main'))
-      .addEventListener('change', () => this.show());
+      .addEventListener('change', () => this.#append());
 
     window.addEventListener('scroll', () => {
       const isScrollAtBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight;
       if (isScrollAtBottom) {
         console.log('scroll at bottom');
-        this.show();
+        this.#append();
       }
     });
 
-    addResizeListener(() => this.show());
+    addResizeListener(() => this.#append());
+  }
+
+  async #append() {
+    for (let i = 0; i < Number.MAX_SAFE_INTEGER; i++) {
+      if (this.#opusList.length < 1) break;
+
+      const lastArticleTop = document.querySelector('main > div:last-child')?.getBoundingClientRect().top || 0;
+      if (lastArticleTop > window.innerHeight) break;
+
+      const opus = Math.random() > 0.5 ? this.#opusList.shift() : this.#opusList.splice(getRandomInt(0, this.#opusList.length), 1)[0];
+      document
+        .querySelector('main')
+        .appendChild(new FlayArticle({ mode: 'cover' }))
+        .set(await FlayFetch.getFlay(opus));
+
+      document.querySelector('#flayCount').innerHTML = this.#opusList.length;
+    }
   }
 }
 

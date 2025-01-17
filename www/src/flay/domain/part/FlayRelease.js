77e4d39cf4d01f2013dc8941e7623c6ab8105e40
Crazy JK <Crazy.4.JK@gmail.com>
2025-01-17 22:58:04 +0900

    shot date

--- www/src/flay/domain/part/FlayRelease.js
+++ www/src/flay/domain/part/FlayRelease.js
@@ -10,10 +10,11 @@ export default class FlayRelease extends FlayHTMLElement {
     super();
 
     this.innerHTML = `
-      <label class="release-label" >                  <span id="release" >0000.00.00</span></label>
-      <label class="modified-label"><sub>mod    </sub><span id="modified">00/00/00</span></label>
-      <label class="access-label"  ><sub>access </sub><span id="access"  >00/00/00</span></label>
-      <label class="played-label"  ><sub>play   </sub><span id="played"  >00/00/00</span></label>
+      <label class=" release-label">                        <span id="release" >0000.00.00</span></label>
+      <label class="modified-label extra"><sub>mod    </sub><span id="modified">00/00/00</span></label>
+      <label class="  access-label extra"><sub>access </sub><span id="access"  >00/00/00</span></label>
+      <label class="  played-label extra"><sub>play   </sub><span id="played"  >00/00/00</span></label>
+      <label class="  shoted-label extra"><sub>shot   </sub><span id="shoted"  >00/00/00</span></label>
     `;
   }
 
@@ -32,6 +33,7 @@ export default class FlayRelease extends FlayHTMLElement {
     this.querySelector('#modified').innerHTML = DateUtils.format(flay.lastModified, 'yy/MM/dd');
     this.querySelector('#access').innerHTML = DateUtils.format(flay.video.lastAccess, 'yy/MM/dd');
     this.querySelector('#played').innerHTML = DateUtils.format(flay.video.lastPlay, 'yy/MM/dd');
+    this.querySelector('#shoted').innerHTML = DateUtils.format(flay.video.likes?.pop(), 'yy/MM/dd');
   }
 }
 
--- www/src/lib/DateUtils.js
+++ www/src/lib/DateUtils.js
@@ -5,24 +5,24 @@ export default class DateUtils {
    * @param {string} pattern default yyyy-MM-dd HH:mm:ss
    * @returns
    */
-  static format(time, pattern = 'yyyy-MM-dd HH:mm:ss') {
+  static format(time = -1, pattern = 'yyyy-MM-dd HH:mm:ss') {
     if (time < 0 || time === '') {
-      return pattern.replace(/yyyy/gi, '0000').replace(/yy/gi, '00').replace(/MM/gi, '00').replace(/M/gi, '0').replace(/dd/gi, '00').replace(/d/gi, '0');
+      return pattern.replace(/yyyy/gi, '0000').replace(/yy/gi, '00').replace(/MM/gi, '00').replace(/M/gi, '0').replace(/dd/gi, '00').replace(/d/gi, '0').replace(/HH/g, '00').replace(/H/g, '0').replace(/mm/g, '00').replace(/m/g, '0').replace(/ss/g, '00').replace(/s/g, '0');
     } else {
       const date = this.getDate(time);
-      const [year, month, day, hour, minute, second] = [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
+      const [fullYear, year, month, day, hour, minute, second] = [date.getFullYear(), date.getFullYear() - 2000, date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
       return pattern
-        .replace(/yyyy/g, year)
-        .replace(/yy/g, year - 2000)
-        .replace(/MM/g, twoDigit(month))
+        .replace(/yyyy/g, fullYear)
+        .replace(/yy/g, String(year).padStart(2, '0'))
+        .replace(/MM/g, String(month).padStart(2, '0'))
         .replace(/M/g, month)
-        .replace(/dd/g, twoDigit(day))
+        .replace(/dd/g, String(day).padStart(2, '0'))
         .replace(/d/g, day)
-        .replace(/HH/g, twoDigit(hour))
+        .replace(/HH/g, String(hour).padStart(2, '0'))
         .replace(/H/g, hour)
-        .replace(/mm/g, twoDigit(minute))
+        .replace(/mm/g, String(minute).padStart(2, '0'))
         .replace(/m/g, minute)
-        .replace(/ss/g, twoDigit(second))
+        .replace(/ss/g, String(second).padStart(2, '0'))
         .replace(/s/g, second);
     }
   }
@@ -47,8 +47,4 @@ export default class DateUtils {
         return value;
     }
   }
-}
-
-function twoDigit(n) {
-  return n > 9 ? n : '0' + n;
 }

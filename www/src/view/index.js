5f8295ae2b11f87dbff445e7d45c98d3c47dd2ff
Crazy JK <Crazy.4.JK@gmail.com>
2025-01-23 22:47:06 +0900

    ModalWindow FlayBasket

--- www/src/view/index.js
+++ www/src/view/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 
 import ModalWindow from '../ui/ModalWindow';
 
+import { FlayBasket } from '../flay/panel/FlayBasket';
 import FlayMemoEditor from '../flay/panel/FlayMemoEditor';
 import FlayVideoViewPanel from '../flay/panel/FlayVideoViewPanel';
 import PopoutCover from '../flay/panel/PopoutCover';
@@ -15,40 +16,67 @@ class Page {
   async start() {
     const [wUnit, hUnit] = [window.innerWidth / 12, window.innerHeight / 12];
 
-    const modalWindow1 = new ModalWindow('Today`s Flay', {
-      top: 0,
-      left: 0,
-      width: getRandomInt(0, wUnit) + wUnit * 4,
-      height: getRandomInt(0, hUnit) + hUnit * 4,
-      edges: 'bottom,left',
-    });
-    const modalWindow2 = new ModalWindow('Image Fallen', {
-      top: 0,
-      left: 0,
-      width: getRandomInt(0, wUnit) + wUnit,
-      height: getRandomInt(0, hUnit) + hUnit * 8,
-      edges: 'top,right',
-    });
-    const modalWindow3 = new ModalWindow('Video', {
-      top: 0,
-      left: 0,
-      width: getRandomInt(0, wUnit) + wUnit * 6,
-      height: getRandomInt(0, hUnit) + hUnit * 6,
-      edges: 'top,left',
-    });
-    const modalWindow4 = new ModalWindow('Memo', {
-      top: 60,
-      left: 0,
-      width: getRandomInt(0, wUnit) + wUnit * 4,
-      height: getRandomInt(0, hUnit) + hUnit * 2,
-      edges: 'bottom,right',
-    });
-
     const main = document.querySelector('body > main');
-    main.appendChild(modalWindow1).appendChild(new PopoutCover());
-    main.appendChild(modalWindow2).appendChild(new ImageFall({ mode: 'random' }));
-    main.appendChild(modalWindow3).appendChild(new FlayVideoViewPanel());
-    main.appendChild(modalWindow4).appendChild(new FlayMemoEditor());
+
+    main
+      .appendChild(
+        new ModalWindow('Video', {
+          top: 0,
+          left: 0,
+          width: getRandomInt(0, wUnit) + wUnit * 8,
+          height: getRandomInt(0, hUnit) + hUnit * 4,
+          edges: 'top,left',
+        })
+      )
+      .appendChild(new FlayVideoViewPanel());
+
+    main
+      .appendChild(
+        new ModalWindow('Basket', {
+          top: 0,
+          left: 0,
+          width: getRandomInt(0, wUnit) + wUnit * 8,
+          height: getRandomInt(0, hUnit) + hUnit * 6,
+          edges: 'bottom,left',
+        })
+      )
+      .appendChild(new FlayBasket());
+
+    main
+      .appendChild(
+        new ModalWindow('Image Fallen', {
+          top: 0,
+          left: 0,
+          width: getRandomInt(0, wUnit) + wUnit * 2,
+          height: getRandomInt(0, hUnit) + hUnit * 7,
+          edges: 'top,right',
+        })
+      )
+      .appendChild(new ImageFall({ mode: 'random' }));
+
+    main
+      .appendChild(
+        new ModalWindow('Cover', {
+          top: getRandomInt(0, hUnit) + hUnit * 7,
+          left: 0,
+          width: getRandomInt(0, wUnit) + wUnit * 4,
+          height: getRandomInt(0, hUnit) + hUnit * 2,
+          edges: 'right',
+        })
+      )
+      .appendChild(new PopoutCover());
+
+    main
+      .appendChild(
+        new ModalWindow('Memo', {
+          top: 0,
+          left: 0,
+          width: getRandomInt(0, wUnit) + wUnit * 4,
+          height: getRandomInt(0, hUnit) + hUnit * 2,
+          edges: 'bottom,right',
+        })
+      )
+      .appendChild(new FlayMemoEditor());
   }
 }
 

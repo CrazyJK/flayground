a4b62cb36f179d0238467ee2ad97c687b813352b
Crazy JK <Crazy.4.JK@gmail.com>
2025-01-31 20:38:13 +0900

    add Timer

--- www/src/ui/Countdown.js
+++ www/src/ui/Countdown.js
@@ -4,6 +4,7 @@ export const EVENT_COUNTDOWN_END = 'countdown-end';
 export const EVENT_COUNTDOWN_RESET = 'countdown-reset';
 export const EVENT_COUNTDOWN_PAUSE = 'countdown-pause';
 export const EVENT_COUNTDOWN_RESUME = 'countdown-resume';
+export const EVENT_COUNTDOWN_CHANGE = 'countdown-change';
 
 /**
  * 카운트다운 타이머를 표시하는 커스텀 요소.
@@ -34,6 +35,7 @@ export class Countdown extends HTMLElement {
   #elapsedTime;
   #animationFrame;
   #isPaused;
+  #prevSeconds = -1;
 
   constructor() {
     super();
@@ -113,9 +115,14 @@ export class Countdown extends HTMLElement {
     const elapsed = performance.now() - this.#startTime + this.#elapsedTime;
     const progress = Math.min(elapsed / this.#duration, 1);
     const degrees = progress * 360;
+    const seconds = Math.ceil((this.#duration - elapsed) / 1000);
 
-    this.dataset.seconds = Math.ceil((this.#duration - elapsed) / 1000);
     this.#circle.style.transform = `rotate(${degrees}deg)`;
+    if (seconds !== this.#prevSeconds) {
+      this.dataset.seconds = seconds;
+      this.dispatchEvent(new CustomEvent(EVENT_COUNTDOWN_CHANGE, { detail: { seconds } }));
+      this.#prevSeconds = seconds;
+    }
 
     if (progress < 1) {
       this.#animationFrame = requestAnimationFrame(() => this.#animate());
@@ -126,3 +133,82 @@ export class Countdown extends HTMLElement {
 }
 
 customElements.define('count-down', Countdown);
+
+export const EVENT_TIMER_START = 'timer-start';
+export const EVENT_TIMER_END = 'timer-end';
+export const EVENT_TIMER_TICK = 'timer-tick';
+
+/**
+ * 초를 입력받아 시간을 재주는 클래스.
+ *
+ * @class Timer
+ * @extends {HTMLElement}
+ * @private {number} #seconds - 타이머 시간(초).
+ * @private {number} #intervalId - setInterval ID.
+ *
+ * @method connectedCallback - 요소가 DOM에 추가될 때 호출됨.
+ * @method disconnectedCallback - 요소가 DOM에서 제거될 때 호출됨.
+ * @method start - 타이머를 시작함.
+ * @method stop - 타이머를 중지함.
+ * @method #tick - 타이머의 매 초마다 호출됨.
+ */
+export class Timer extends HTMLElement {
+  #seconds;
+  #intervalId;
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          font-size: 2em;
+          text-align: center;
+        }
+      </style>
+      <div id="time"></div>
+    `;
+    this.#seconds = 0;
+    this.#intervalId = null;
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  disconnectedCallback() {
+    this.stop();
+  }
+
+  start(seconds) {
+    this.#seconds = seconds;
+    this.stop();
+    this.#intervalId = setInterval(() => this.#tick(), 1000);
+    this.dispatchEvent(new Event(EVENT_TIMER_START));
+  }
+
+  stop() {
+    if (this.#intervalId !== null) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = null;
+    }
+  }
+
+  #tick() {
+    if (this.#seconds > 0) {
+      this.#seconds--;
+      this.render();
+      this.dispatchEvent(new CustomEvent(EVENT_TIMER_TICK, { detail: { seconds: this.#seconds } }));
+    } else {
+      this.stop();
+      this.dispatchEvent(new Event(EVENT_TIMER_END));
+    }
+  }
+
+  render() {
+    this.shadowRoot.getElementById('time').textContent = this.#seconds;
+  }
+}
+
+customElements.define('simple-timer', Timer);

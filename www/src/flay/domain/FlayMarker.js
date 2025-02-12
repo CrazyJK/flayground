831868e91234f9d3464e66582d451b95ce67f4e9
Crazy JK <Crazy.4.JK@gmail.com>
2025-02-12 18:45:39 +0900

    refactor FlayMarker to implement default options and enhance cover display functionality

--- www/src/flay/domain/FlayMarker.js
+++ www/src/flay/domain/FlayMarker.js
@@ -1,9 +1,13 @@
 import { popupFlay } from '../../lib/FlaySearch';
 import './FlayMarker.scss';
 
+const DEFAULT_OPTIONS = { showTitle: true, showCover: false };
+
 export default class FlayMarker extends HTMLLabelElement {
-  constructor(flay, options = { showTitle: true }) {
+  constructor(flay, options = DEFAULT_OPTIONS) {
     super();
+
+    options = { ...DEFAULT_OPTIONS, ...options };
 
     this.flay = flay;
     this.classList.add('flay-marker');
@@ -14,9 +18,18 @@ export default class FlayMarker extends HTMLLabelElement {
       this.classList.add('active');
     });
 
+    if (options.showCover) {
+      this.addEventListener('mouseover', () => this.showCover(), { once: true });
+    }
+
     if (options.showTitle) {
       this.title = `${flay.studio}\n${flay.opus}\n${flay.title}\n${flay.actressList.join(', ')}\n${flay.release}`;
     }
+  }
+
+  showCover() {
+    this.classList.add('cover');
+    this.style.backgroundImage = `url(/static/cover/${this.flay.opus})`;
   }
 }
 

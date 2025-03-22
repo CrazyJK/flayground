6057d79987b3eb1c8ddd65a50aa3edda91ebcf3b
Crazy JK <Crazy.4.JK@gmail.com>
2025-03-22 23:19:24 +0900

    add click event to randomly select FlayMarkerPanel marker on main area click

--- www/src/view/index.js
+++ www/src/view/index.js
@@ -7,6 +7,15 @@ class Page {
 
   async start() {
     document.querySelector('body > main').appendChild(new FlayMarkerPanel());
+    // body > main 빈공간을 클릭했을때 이벤트
+    document.querySelector('body > main').addEventListener('click', (e) => {
+      if (e.target === document.querySelector('body > main')) {
+        // FlayMarkerPanel의 marker를 랜덤으로 선택해서 클릭
+        const flayMarkerList = document.querySelectorAll('.flay-marker-panel .flay-marker');
+        const randomIndex = Math.floor(Math.random() * flayMarkerList.length);
+        flayMarkerList[randomIndex].click();
+      }
+    });
   }
 }
 

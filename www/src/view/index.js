306d9b18c28087e7b74e3ba9e6f73d7f845fea67
Crazy JK <Crazy.4.JK@gmail.com>
2025-03-25 23:20:35 +0900

    refactor click event to randomly select FlayMarkerPanel marker based on mouse position

--- www/src/view/index.js
+++ www/src/view/index.js
@@ -7,14 +7,14 @@ class Page {
 
   async start() {
     document.querySelector('body > main').appendChild(new FlayMarkerPanel());
-    // body > main 빈공간을 클릭했을때 이벤트
+
     document.querySelector('body > main').addEventListener('click', (e) => {
-      if (e.target === document.querySelector('body > main')) {
-        // FlayMarkerPanel의 marker를 랜덤으로 선택해서 클릭
-        const flayMarkerList = document.querySelectorAll('.flay-marker-panel .flay-marker');
-        const randomIndex = Math.floor(Math.random() * flayMarkerList.length);
-        flayMarkerList[randomIndex].click();
-      }
+      if (e.target !== document.querySelector('body > main')) return;
+
+      const flayMarkerList = document.querySelectorAll('.flay-marker-panel .flay-marker');
+      const inputNumber = e.clientX * e.clientY;
+      const randomIndex = inputNumber % flayMarkerList.length;
+      flayMarkerList[randomIndex].click();
     });
   }
 }

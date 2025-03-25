2b7607f1b5d9ada9183c5794c93f8f2bd10e5f2b
Crazy JK <Crazy.4.JK@gmail.com>
2025-03-25 23:30:10 +0900

    refactor event handling to improve marker selection logic in FlayMarkerPanel

--- www/src/view/index.js
+++ www/src/view/index.js
@@ -1,20 +1,20 @@
-import { FlayMarkerPanel } from '../flay/panel/FlayMarkerPanel';
 import './inc/Page';
 import './index.scss';
+
+import { FlayMarkerPanel } from '../flay/panel/FlayMarkerPanel';
 
 class Page {
   constructor() {}
 
   async start() {
-    document.querySelector('body > main').appendChild(new FlayMarkerPanel());
-
-    document.querySelector('body > main').addEventListener('click', (e) => {
-      if (e.target !== document.querySelector('body > main')) return;
-
-      const flayMarkerList = document.querySelectorAll('.flay-marker-panel .flay-marker');
+    const mainElement = document.querySelector('body > main');
+    mainElement.appendChild(new FlayMarkerPanel());
+    mainElement.addEventListener('click', (e) => {
+      if (e.target !== mainElement) return;
+      const flayMarkers = document.querySelectorAll('.flay-marker-panel .flay-marker');
       const inputNumber = e.clientX * e.clientY;
-      const randomIndex = inputNumber % flayMarkerList.length;
-      flayMarkerList[randomIndex].click();
+      const randomIndex = inputNumber % flayMarkers.length;
+      flayMarkers[randomIndex].click();
     });
   }
 }

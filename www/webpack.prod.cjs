afd27d79fb59add617610ffaf386e266b8d033cb
Crazy JK <Crazy.4.JK@gmail.com>
2025-05-04 12:51:00 +0900

    feat: madge 및 appendChunkhash 스크립트를 실행하는 플러그인 추가

--- www/webpack.prod.cjs
+++ www/webpack.prod.cjs
@@ -1,4 +1,37 @@
 const WebpackManifestPlugin = require('webpack-manifest-plugin').WebpackManifestPlugin;
+const { exec } = require('child_process');
+
+// 의존성 다이어그램을 생성하는 플러그인
+class MadgePlugin {
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('MadgePlugin', () => {
+      console.log('\n🔍 Running madge to generate dependency diagrams...');
+      exec('node madge.cjs', { cwd: __dirname }, (error, stdout) => {
+        if (error) {
+          console.error(`Error running madge: ${error}`);
+          return;
+        }
+        console.log(stdout);
+      });
+    });
+  }
+}
+
+// Chunkhash를 추가하는 플러그인
+class AppendChunkhashPlugin {
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('AppendChunkhashPlugin', () => {
+      console.log('\n🔄 Running appendChunkhash to update HTML files...');
+      exec('node appendChunkhash.cjs', { cwd: __dirname }, (error, stdout) => {
+        if (error) {
+          console.error(`Error running appendChunkhash: ${error}`);
+          return;
+        }
+        console.log(stdout);
+      });
+    });
+  }
+}
 
 module.exports = {
   mode: 'production',
@@ -10,6 +43,8 @@ module.exports = {
     new WebpackManifestPlugin({
       filter: (file) => file.name.endsWith('.js'),
     }),
+    new MadgePlugin(), // madge.cjs 스크립트를 실행하는 플러그인
+    new AppendChunkhashPlugin(), // appendChunkhash.cjs 스크립트를 실행하는 플러그인 추가
   ],
   module: {
     rules: [

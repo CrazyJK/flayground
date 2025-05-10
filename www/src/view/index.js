431fed4f9d1cf4959cb7bc62b53962b5a4435aab
Crazy JK <Crazy.4.JK@gmail.com>
2025-05-10 16:15:37 +0900

    feat: FlayMarker 및 FlayFetch 추가로 마커 패널 기능 개선

--- www/src/view/index.js
+++ www/src/view/index.js
@@ -1,3 +1,5 @@
+import FlayMarker from '@/flay/domain/FlayMarker';
+import FlayFetch from '@/lib/FlayFetch';
 import DateUtils from '@lib/DateUtils';
 import './inc/Page';
 import './index.scss';
@@ -15,17 +17,44 @@ class Page {
 
   #showMarkerPanel() {
     this.#mainElement.textContent = null;
-    import(/* webpackChunkName: "FlayMarkerPanel" */ '@flay/panel/FlayMarkerPanel')
-      .then(({ FlayMarkerPanel }) => this.#mainElement.appendChild(new FlayMarkerPanel()))
-      .then((flayMarkerPanel) => {
-        this.#mainElement.addEventListener('click', (e) => {
-          if (e.target !== this.#mainElement) return;
-          const inputNumber = e.clientX * e.clientY;
-          const flayMarkers = flayMarkerPanel.childNodes;
-          const randomIndex = inputNumber % flayMarkers.length;
-          flayMarkers[randomIndex].click();
+
+    // 랜덤 boolean
+    const randomBoolean = Math.random() < 0.5;
+    if (randomBoolean) {
+      import(/* webpackChunkName: "FlayMarkerPanel" */ '@flay/panel/FlayMarkerPanel')
+        .then(({ FlayMarkerPanel }) => this.#mainElement.appendChild(new FlayMarkerPanel()))
+        .then((flayMarkerPanel) => {
+          this.#mainElement.addEventListener('click', (e) => {
+            if (e.target !== this.#mainElement) return;
+            const inputNumber = e.clientX * e.clientY;
+            const flayMarkers = flayMarkerPanel.childNodes;
+            const randomIndex = inputNumber % flayMarkers.length;
+            flayMarkers[randomIndex].click();
+          });
         });
+    } else {
+      FlayFetch.getFlayAll().then(async (list) => {
+        const [min, max] = [20, 50];
+        const mainElement = document.querySelector('body > main');
+
+        for (const flay of list) {
+          const x = Math.floor(Math.random() * (window.innerWidth - (min + max))) + min;
+          const y = Math.floor(Math.random() * (window.innerHeight - (min + max))) + min;
+          const randomWidth = Math.floor(Math.random() * (max - min)) + min;
+
+          const flayMarker = new FlayMarker(flay, { showTitle: false, shape: 'star' });
+          flayMarker.style.position = 'absolute';
+          flayMarker.style.zIndex = 1000;
+          flayMarker.style.left = `${x}px`;
+          flayMarker.style.top = `${y}px`;
+          flayMarker.style.width = `${randomWidth}px`;
+          flayMarker.animate([{ transform: 'scale(0)' }, { transform: 'scale(1)' }], { duration: 500, easing: 'ease-in-out' });
+          mainElement.appendChild(flayMarker);
+
+          await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * 700) + 300)); // 300ms~1000ms 대기
+        }
       });
+    }
   }
 }
 

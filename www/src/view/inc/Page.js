5f602bcd49f54431e0413b2ba29abfebfde12c9a
Crazy JK <Crazy.4.JK@gmail.com>
2025-05-11 15:40:58 +0900

    fix: Page.js에서 배경 이미지 설정 위치 수정 및 Popup.js에서 중복된 SseConnector 임포트 제거

--- www/src/view/inc/Page.js
+++ www/src/view/inc/Page.js
@@ -2,8 +2,8 @@ import '@lib/SseConnector';
 import '@lib/UpdateMyPosition';
 import './Page.scss';
 
-document.body.style.backgroundImage = 'url(./svg/flayground-circle.svg)';
-
 import(/* webpackChunkName: "SideNavBar" */ '@nav/SideNavBar').then(({ SideNavBar }) => {
   document.body.prepend(new SideNavBar());
 });
+
+document.body.style.backgroundImage = 'url(./svg/flayground-circle.svg)';

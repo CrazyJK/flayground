a81a40a1fbfb872dc2e1d084a5aebb10d3909dec
Crazy JK <Crazy.4.JK@gmail.com>
2025-05-27 22:41:29 +0900

    feat: 이미지 패널 크기 및 스타일 조정, 이미지 표시 로직 개선

--- www/src/view/inc/Page.js
+++ www/src/view/inc/Page.js
@@ -12,7 +12,7 @@ document.body.style.backgroundImage = 'url(./svg/flayground-circle.svg)';
 const IMAGE_CHANGE_INTERVAL = 10000; // 이미지 변경 간격 (10초)
 
 FlayFetch.getImageSize()
-  .then((imageLength) => {
+  .then(async (imageLength) => {
     if (imageLength === 0) {
       console.warn('표시할 이미지가 없습니다.');
       return;
@@ -26,24 +26,28 @@ FlayFetch.getImageSize()
 
     let imageURL; // 이전 이미지 URL을 저장하기 위한 변수
 
-    const showImage = () => {
+    const showImage = (randomSize) => {
       if (imageURL) {
-        // 이전 imageURL이 있다면 해제
-        URL.revokeObjectURL(imageURL);
+        URL.revokeObjectURL(imageURL); // 이전 imageURL이 있다면 해제
       }
 
       if (imageIndices.length === 0) {
         imageIndices = [...originalImageIndices]; // 모든 이미지가 표시되었으면 원본 복사본으로 초기화
         console.log('모든 이미지를 표시하여 목록을 초기화합니다.');
       }
+
       const randomIndex = Math.floor(Math.random() * imageIndices.length);
       const idx = imageIndices.splice(randomIndex, 1)[0];
 
       FlayFetch.getStaticImage(idx)
         .then(({ name, path, modified, imageBlob }) => {
           imageURL = URL.createObjectURL(imageBlob);
-          imagePanel.title = `${name} (modified: ${modified}, path: ${path})`; // title에 추가 정보 제공
+          imagePanel.dataset.size = randomSize;
+          imagePanel.title = `${name}\n${modified}\n${path}`; // title에 추가 정보 제공
           imagePanel.style.backgroundImage = `url(${imageURL})`;
+          imagePanel.style.width = randomSize + 'rem';
+          imagePanel.style.height = randomSize + 'rem';
+          imagePanel.style.margin = (10 - randomSize) / 2 + 'rem';
           imagePanel.animate([{ transform: 'scale(0.1)' }, { transform: 'scale(1.05)' }, { transform: 'scale(1)' }], { duration: 2000, easing: 'ease-in-out' });
         })
         .catch((error) => {
@@ -56,8 +60,12 @@ FlayFetch.getImageSize()
         });
     };
 
-    showImage(); // 페이지 로드 시 첫 이미지 즉시 표시
-    setInterval(showImage, IMAGE_CHANGE_INTERVAL);
+    const condition = true; // 조건을 true로 설정하여 무한 루프를 방지
+    do {
+      const randomSize = 5 + Math.floor(Math.random() * 9) * 0.5; // 5rem에서 9rem 사이의 크기
+      showImage(randomSize);
+      await new Promise((resolve) => setTimeout(resolve, randomSize * 1000)); // 이미지 변경 간격만큼 대기
+    } while (condition);
   })
   .catch((error) => {
     console.error('이미지 갯수를 가져오는 중 오류 발생:', error);

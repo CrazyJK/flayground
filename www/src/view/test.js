c802fd8f75ebf59e6cb3933739be1049b47525de
Crazy JK <Crazy.4.JK@gmail.com>
2025-06-03 14:14:20 +0900

    fix: 이미지 원형 변경 및 크기 조정으로 시각적 일관성 개선

--- www/src/view/test.js
+++ www/src/view/test.js
@@ -20,7 +20,7 @@ class Page {
     mainElement.style.justifyContent = 'center';
     mainElement.style.alignItems = 'center';
     mainElement.style.height = '100vh';
-    mainElement.appendChild(new ImageCircle({ rem: 40, shape: 'rounded', effect: 'emboss', duration: 2000 }));
+    mainElement.appendChild(new ImageCircle({ rem: 50, shape: 'circle', effect: 'emboss', duration: 2000 }));
 
     document.querySelector('body > footer').appendChild(newHTMLButtonElement('button', 'Video', () => this.#videoWindow(wUnit, hUnit)));
     document.querySelector('body > footer').appendChild(newHTMLButtonElement('button', 'Basket', () => this.#basketWindow(wUnit, hUnit)));

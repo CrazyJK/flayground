58226dd92161868ad238ba7ab57fcf07da9fda76
Crazy JK <Crazy.4.JK@gmail.com>
2025-06-04 16:53:48 +0900

    fix: ImageCircle 클릭 이벤트 및 리사이즈 처리 추가, 스타일 조정

--- www/src/view/index.js
+++ www/src/view/index.js
@@ -8,12 +8,24 @@ class Page {
   async start() {
     const mainElement = document.querySelector('body > main');
     import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
-      .then(({ ImageCircle }) => new ImageCircle({ effect: 'engrave', eventAllow: true }))
+      .then(({ ImageCircle }) => new ImageCircle({ effect: 'engrave' }))
       .then((imageCircle) => {
         imageCircle.style.position = 'fixed';
         imageCircle.style.right = 0;
         imageCircle.style.bottom = 0;
         mainElement.appendChild(imageCircle);
+
+        imageCircle.addEventListener('click', () => {
+          imageCircle.style.position = 'relative';
+          imageCircle.style.right = 'unset';
+          imageCircle.style.bottom = 'unset';
+          window.dispatchEvent(new Event('resize'));
+        });
+        window.addEventListener('resize', () => {
+          if (imageCircle.style.position === 'fixed') return; // 고정 위치일 때는 무시
+          const rem = getAvailableRemSize(mainElement);
+          imageCircle.setOptions({ rem: rem, effect: 'emboss', duration: 3000, eventAllow: true });
+        });
       });
   }
 }
@@ -21,3 +33,29 @@ class Page {
 new Page().start();
 
 console.info(`%c\n\tFlayground : ${process.env.NODE_ENV} : ${process.env.WATCH_MODE === true ? 'Watch mode' : ''} 🕒 ${DateUtils.format(process.env.BUILD_TIME)}\n`, 'color: orange; font-size: 20px; font-weight: bold;');
+
+function getAvailableRemSize(element) {
+  function getAvailableWidth(element) {
+    const computedStyle = getComputedStyle(element);
+    const paddingLeft = parseFloat(computedStyle.paddingLeft);
+    const paddingRight = parseFloat(computedStyle.paddingRight);
+
+    return window.innerWidth - paddingLeft - paddingRight;
+  }
+  function getAvailableHeight(element) {
+    const computedStyle = getComputedStyle(element);
+    const paddingTop = parseFloat(computedStyle.paddingTop);
+    const paddingBottom = parseFloat(computedStyle.paddingBottom);
+
+    return window.innerHeight - paddingTop - paddingBottom;
+  }
+
+  function pxToRem(px) {
+    const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+    return Math.floor(px / rem);
+  }
+
+  const width = getAvailableWidth(element);
+  const height = getAvailableHeight(element);
+  return Math.min(pxToRem(width), pxToRem(height));
+}

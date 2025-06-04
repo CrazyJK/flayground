5c8cf404f651ef258cfc79f8892c7a87b9af8d3e
Crazy JK <Crazy.4.JK@gmail.com>
2025-06-04 14:36:33 +0900

    fix: #showMarkerPanel 메서드 및 클릭 이벤트 리스너 제거

--- www/src/view/index.js
+++ www/src/view/index.js
@@ -10,8 +10,6 @@ class Page {
   }
 
   async start() {
-    this.#mainElement.addEventListener('click', () => this.#showMarkerPanel(), { once: true });
-
     import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
       .then(({ ImageCircle }) => new ImageCircle({ effect: 'engrave', eventAllow: true }))
       .then((imageCircle) => {
@@ -20,30 +18,6 @@ class Page {
         imageCircle.style.bottom = 0;
         this.#mainElement.appendChild(imageCircle);
       });
-  }
-
-  #showMarkerPanel() {
-    const randomBoolean = Math.random() < 0.5;
-    if (randomBoolean) {
-      import(/* webpackChunkName: "FlayMarkerPanel" */ '@flay/panel/FlayMarkerPanel')
-        .then(({ FlayMarkerPanel }) => new FlayMarkerPanel())
-        .then((flayMarkerPanel) => {
-          this.#mainElement.appendChild(flayMarkerPanel);
-          this.#mainElement.addEventListener('click', (e) => {
-            if (e.target !== this.#mainElement) return;
-            const inputNumber = e.clientX * e.clientY;
-            const flayMarkers = flayMarkerPanel.childNodes;
-            const randomIndex = inputNumber % flayMarkers.length;
-            flayMarkers[randomIndex].click();
-          });
-        });
-    } else {
-      import(/* webpackChunkName: "FlayMarkerSky" */ '@flay/panel/FlayMarkerSky')
-        .then(({ FlayMarkerSky }) => new FlayMarkerSky())
-        .then((flayMarkerSky) => {
-          this.#mainElement.appendChild(flayMarkerSky);
-        });
-    }
   }
 }
 

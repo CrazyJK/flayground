190c3a79625b2dc869bb2c2f2f3dfbd69cfa13f6
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-05 12:34:39 +0900

    feat: 원형 마스크의 클립 패스 업데이트 로직 개선 및 CSS 변수를 사용하여 반지름 조정 기능 추가

--- www/src/image/FlayCircleMask.ts
+++ www/src/image/FlayCircleMask.ts
@@ -5,8 +5,8 @@ import './FlayCircleMask.scss';
  * FlayCircleMask
  */
 export class FlayCircleMask extends HTMLDivElement {
-  private offsetX: number = 0; // 마우스 X 좌표
-  private offsetY: number = 0; // 마우스 Y 좌표
+  private rectX: number = 0; // 이 요소의 x 위치
+  private rectY: number = 0; // 이 요소의 y 위치
   private lastMouseX: number = 0; // 마지막 마우스 X 위치
   private lastMouseY: number = 0; // 마지막 마우스 Y 위치
   private radius: number = 0; // 원의 반지름
@@ -49,49 +49,51 @@ export class FlayCircleMask extends HTMLDivElement {
 
     // 마우스가 화면에 들어오면 마스크 표시
     document.addEventListener('mouseenter', (e: MouseEvent) => {
-      this.classList.add('mouse-move');
-      this.updateClipPath(e.clientX, e.clientY);
+      this.updateClipPathProperties(50, e.clientX, e.clientY);
       this.updateLastMousePosition(e.clientX, e.clientY); // 마지막 마우스 위치 업데이트
     });
 
     // 마우스 움직임에 따라 원형 마스크 업데이트 (requestAnimationFrame으로 최적화)
     document.addEventListener('mousemove', (e: MouseEvent) => {
-      this.classList.add('mouse-move');
       if (animationId) cancelAnimationFrame(animationId);
-      animationId = requestAnimationFrame(() => this.updateClipPath(e.clientX, e.clientY));
+      animationId = requestAnimationFrame(() => this.updateClipPathProperties(50, e.clientX, e.clientY));
       this.updateLastMousePosition(e.clientX, e.clientY); // 마지막 마우스 위치 저장
     });
 
     // 마우스가 화면을 벗어나면 마지막 위치에서 원이 작아지면서 사라짐
     document.addEventListener('mouseleave', () => {
       if (animationId) cancelAnimationFrame(animationId);
-      this.classList.remove('mouse-move');
-      this.style.clipPath = `circle(0px at ${this.lastMouseX - this.offsetX}px ${this.lastMouseY - this.offsetY}px)`; // 마지막 마우스 위치에서 원의 크기를 0으로 축소
+      this.updateClipPathProperties(500, this.lastMouseX, this.lastMouseY, 0); // 마지막 마우스 위치에서 원의 크기를 0으로 축소
     });
 
+    // 클릭 시 전체 화면 마스크 토글
     this.addEventListener('click', () => {
       this.classList.toggle('full-mask'); // 클릭 시 전체 화면 마스크 토글
+      this.updateClipPathProperties(500, this.lastMouseX, this.lastMouseY);
     });
 
+    // 휠 스크롤로 원의 반지름 조정
     this.addEventListener('wheel', (e: WheelEvent) => {
       e.preventDefault();
       const delta = e.deltaY > 0 ? 1 : -1; // 휠 스크롤 방향에 따라 반지름 조정
       this.radius = Math.max(0, this.radius + delta * 10);
-      this.updateClipPath(this.lastMouseX, this.lastMouseY); // 현재 마우스 위치에서 클립 패스 업데이트
+      this.updateClipPathProperties(200, this.lastMouseX, this.lastMouseY); // 현재 마우스 위치에서 클립 패스 업데이트
     });
 
-    window.addEventListener('resize', () => this.decideOffsetXY()); // 윈도우 크기 조정 시 오프셋 업데이트
-    this.decideOffsetXY(); // 초기 오프셋 계산
+    // 윈도우 크기 조정 시 오프셋 업데이트
+    window.addEventListener('resize', () => {
+      const { x, y, width, height } = this.getBoundingClientRect(); // 현재 메인 엘리먼트의 위치를 기준으로 오프셋 계산
+      [this.rectX, this.rectY] = [x, y];
+      this.radius = Math.min(width, height) / 4; // 원의 반지름을 메인 엘리먼트의 크기에 따라 설정
+    });
+    window.dispatchEvent(new Event('resize')); // resize 이벤트 일으키기
   }
 
-  private decideOffsetXY(): void {
-    const { x, y, width, height } = this.getBoundingClientRect(); // 현재 메인 엘리먼트의 위치를 기준으로 오프셋 계산
-    [this.offsetX, this.offsetY] = [x, y];
-    this.radius = Math.min(width, height) / 4; // 원의 반지름을 메인 엘리먼트의 크기에 따라 설정
-  }
-
-  private updateClipPath(x: number, y: number): void {
-    this.style.clipPath = `circle(${this.radius}px at ${x - this.offsetX}px ${y - this.offsetY}px)`; // 클립 패스 업데이트
+  private updateClipPathProperties(duration: number, x: number, y: number, radius: number | null = this.radius): void {
+    document.documentElement.style.setProperty('--clip-transition-duration', `${duration}ms`);
+    document.documentElement.style.setProperty('--clip-x', `${x - this.rectX}px`);
+    document.documentElement.style.setProperty('--clip-y', `${y - this.rectY}px`);
+    document.documentElement.style.setProperty('--clip-radius', `${radius}px`);
   }
 
   private updateLastMousePosition(x: number, y: number): void {

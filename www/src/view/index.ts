6a03b0572a4daab2fea73f4c724ad2b4b67e4a5f
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-10 22:46:23 +0900

    feat: 무한 이미지 갤러리 컴포넌트 추가 및 스타일 정의

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -1,2 +1,8 @@
+import '../image/InfiniteImageGallery';
 import './inc/Page';
 import './index.scss';
+
+// 갤러리 커스텀 엘리먼트 생성 및 추가
+const main = document.querySelector('body > main') as HTMLDivElement;
+const gallery = document.createElement('infinite-image-gallery');
+main.appendChild(gallery);

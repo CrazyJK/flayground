7e0f999102cd744804031f5f55a8bab61aed6edb
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-10 18:58:31 +0900

    fix: DateUtils 임포트 및 콘솔 정보 제거

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -1,5 +1,2 @@
-import DateUtils from '@lib/DateUtils';
 import './inc/Page';
 import './index.scss';
-
-console.info(`%c\n\tFlayground : ${process.env.NODE_ENV} : ${process.env.WATCH_MODE === 'true' ? 'Watch mode' : ''} 🕒 ${DateUtils.format(process.env.BUILD_TIME)}\n`, 'color: orange; font-size: 20px; font-weight: bold;');

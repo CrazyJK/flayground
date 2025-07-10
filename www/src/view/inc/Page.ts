f9cd7a8f8dd594c3c248fd68833b0a6ec2f8e972
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-10 18:58:23 +0900

    feat: Page.ts에 DateUtils 임포트 및 콘솔 정보 추가

--- www/src/view/inc/Page.ts
+++ www/src/view/inc/Page.ts
@@ -1,3 +1,4 @@
+import DateUtils from '@lib/DateUtils';
 import '@lib/SseConnector';
 import '@lib/UpdateMyPosition';
 import './Page.scss';
@@ -5,3 +6,5 @@ import './Page.scss';
 import(/* webpackChunkName: "SideNavBar" */ '@nav/SideNavBar').then(({ SideNavBar }) => {
   document.body.prepend(new SideNavBar());
 });
+
+console.info(`%c\n\tFlayground : ${process.env.NODE_ENV} : ${process.env.WATCH_MODE === 'true' ? 'Watch mode' : ''} 🕒 ${DateUtils.format(process.env.BUILD_TIME)}\n`, 'color: orange; font-size: 20px; font-weight: bold;');

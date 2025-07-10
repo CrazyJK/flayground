ae43f5e704a961977e4a1f5af38f85262f09b2a5
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-10 15:47:27 +0900

    feat: 페이지 마스크 HTML 및 SCSS 파일 추가, FlayCircleMask 초기화 코드 포함

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -1,8 +1,5 @@
 import DateUtils from '@lib/DateUtils';
-import { FlayCircleMask } from '../image/FlayCircleMask';
 import './inc/Page';
 import './index.scss';
 
 console.info(`%c\n\tFlayground : ${process.env.NODE_ENV} : ${process.env.WATCH_MODE === 'true' ? 'Watch mode' : ''} 🕒 ${DateUtils.format(process.env.BUILD_TIME)}\n`, 'color: orange; font-size: 20px; font-weight: bold;');
-
-document.querySelector('body').appendChild(new FlayCircleMask());
---
+++ www/src/view/page.image-mask.ts
@@ -0,0 +1,5 @@
+import { FlayCircleMask } from '@/image/FlayCircleMask';
+import './inc/Page';
+import './page.image-mask.scss';
+
+document.querySelector('body').appendChild(new FlayCircleMask());

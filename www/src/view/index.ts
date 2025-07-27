43e96ea3e30f72c6e49f98244d94f179bd187d10
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-27 17:46:41 +0900

    refactor: reorganize async imports for FacadeWebMovie and ImageCircle components

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -1,27 +1,28 @@
 import './inc/Page';
 import './index.scss';
 
-import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
-  .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
-  .then((imageCircle) => {
-    imageCircle.style.position = 'fixed';
-    imageCircle.style.right = '0';
-    imageCircle.style.bottom = '0';
+(async () => {
+  await import(/* webpackChunkName: "FacadeWebMovie" */ '@/movie/FacadeWebMovie')
+    .then(({ FacadeWebMovie }) => new FacadeWebMovie())
+    .then(async (facadeWebMovie) => {
+      document.querySelector('body > main').appendChild(facadeWebMovie);
+      await facadeWebMovie.isEnded();
+    });
 
-    document.head.appendChild(document.createElement('style')).textContent = `
-      .image-circle {
-        opacity: 0.5;
-        transition: opacity 0.3s ease-in-out;
-      }
-      .image-circle:hover {
-        opacity: 1;
-      }`;
-    document.body.appendChild(imageCircle);
-  });
+  import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
+    .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
+    .then((imageCircle) => {
+      document.head.appendChild(document.createElement('style')).textContent = `
+        .image-circle {
+          opacity: 0.5;
+          transition: opacity 0.3s ease-in-out;
+        }
+        .image-circle:hover {
+          opacity: 1;
+        }
+      `;
+      imageCircle.classList.add('right-bottom');
 
-import(/* webpackChunkName: "FacadeWebMovie" */ '@/movie/FacadeWebMovie')
-  .then(({ FacadeWebMovie }) => new FacadeWebMovie())
-  .then(async (facadeWebMovie) => {
-    document.querySelector('body > main').appendChild(facadeWebMovie);
-    await facadeWebMovie.isEnded();
-  });
+      document.body.appendChild(imageCircle);
+    });
+})();

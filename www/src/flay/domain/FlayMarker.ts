fa697f006afd368bed9a086e335a6ea05d5cc938
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-28 22:41:58 +0900

    fix: refactor FlayMarker class to improve event handling and encapsulate methods

--- www/src/flay/domain/FlayMarker.ts
+++ www/src/flay/domain/FlayMarker.ts
@@ -41,20 +41,32 @@ export default class FlayMarker extends HTMLLabelElement {
    */
   constructor(flay: Flay, options: Partial<FlayMarkerOptions> = {}) {
     super();
+    this.set(flay, options);
+  }
 
+  connectedCallback(): void {
+    this.addEventListener('click', this.#clickHandler.bind(this));
+  }
+
+  disconnectedCallback(): void {
+    this.removeEventListener('click', this.#clickHandler.bind(this));
+    this.removeEventListener('mouseover', this.#mouseoverHandler.bind(this));
+  }
+
+  set(flay: Flay, options: Partial<FlayMarkerOptions> = {}): void {
     const mergedOptions: FlayMarkerOptions = { ...DEFAULT_OPTIONS, ...options };
 
     this.flay = flay;
     this.classList.add('flay-marker');
     this.classList.toggle('shot', flay.video.likes?.length > 0);
     this.classList.toggle('archive', flay.archive);
-    this.addEventListener('click', () => {
-      popupFlay(flay.opus);
-      this.classList.add('active');
-    });
+
+    this.classList.remove('square', 'circle', 'star', 'heart', 'rhombus');
+    this.innerHTML = ''; // Clear previous content
+    this.title = '';
 
     if (mergedOptions.showCover) {
-      this.addEventListener('mouseover', () => this.showCover(), { once: true });
+      this.addEventListener('mouseover', this.#mouseoverHandler.bind(this), { once: true });
     }
     if (mergedOptions.showTitle) {
       this.title = `${flay.studio}\n${flay.opus}\n${flay.title}\n${flay.actressList.join(', ')}\n${flay.release}\n${flay.video.rank}R`;
@@ -66,6 +78,19 @@ export default class FlayMarker extends HTMLLabelElement {
     } else if (mergedOptions.shape === 'star') {
       this.innerHTML = ranks[flay.video.rank + 1];
     }
+  }
+
+  /**
+   * 클릭 이벤트 핸들러
+   * - Flay 팝업 표시
+   */
+  #clickHandler(): void {
+    popupFlay(this.flay.opus);
+    this.classList.add('active');
+  }
+
+  #mouseoverHandler(): void {
+    this.showCover();
   }
 
   /**

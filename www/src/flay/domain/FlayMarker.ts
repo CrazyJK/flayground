77e31191fb612db8b6c398fc7ec735917932ad7f
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-29 14:05:35 +0900

    fix: update FlayMarker component to use tooltip options and remove cover image handling

--- www/src/flay/domain/FlayMarker.ts
+++ www/src/flay/domain/FlayMarker.ts
@@ -1,26 +1,24 @@
-import ApiClient from '@lib/ApiClient';
-import { Flay } from '@lib/FlayFetch';
 import { popupFlay } from '@lib/FlaySearch';
 import favorite from '@svg/favorite';
 import ranks from '@svg/ranks';
 import './FlayMarker.scss';
+import { FlayTooltip } from '@flay/domain/FlayTooltip';
+import { Flay } from '@lib/FlayFetch';
+import StyleUtils from '../../lib/StyleUtils';
 
 export type ShapeType = 'square' | 'circle' | 'star' | 'heart' | 'rhombus';
 
 /** FlayMarker 컴포넌트 옵션 */
 export interface FlayMarkerOptions {
-  /** 제목 툴팁 표시 여부 */
-  showTitle: boolean;
-  /** 커버 이미지 표시 여부 */
-  showCover: boolean;
+  /** Flay 툴팁 표시 여부 */
+  tooltip: boolean;
   /** 마커 모양 */
   shape: ShapeType;
 }
 
 /** 기본 옵션 */
 const DEFAULT_OPTIONS: FlayMarkerOptions = {
-  showTitle: true,
-  showCover: false,
+  tooltip: false,
   shape: 'square',
 };
 
@@ -28,11 +26,12 @@ const DEFAULT_OPTIONS: FlayMarkerOptions = {
  * Flay 정보를 표시하는 마커 컴포넌트
  * - 다양한 모양 지원 (square, circle, star, heart, rhombus)
  * - 클릭 시 Flay 팝업 표시
- * - 호버 시 커버 이미지 표시 옵션
+ * - 호버 시 툴팁 표시 옵션
  */
 export default class FlayMarker extends HTMLLabelElement {
   /** Flay 데이터 */
   flay: Flay;
+  #options: FlayMarkerOptions;
 
   /**
    * FlayMarker 생성자
@@ -45,42 +44,43 @@ export default class FlayMarker extends HTMLLabelElement {
   }
 
   connectedCallback(): void {
-    this.addEventListener('click', this.#clickHandler.bind(this));
+    this.addEventListener('click', this.#popupFlayHandler.bind(this));
+    this.addEventListener('mouseover', this.#showTooltipHandler.bind(this));
   }
 
   disconnectedCallback(): void {
-    this.removeEventListener('click', this.#clickHandler.bind(this));
-    this.removeEventListener('mouseover', this.#mouseoverHandler.bind(this));
+    this.removeEventListener('click', this.#popupFlayHandler.bind(this));
+    this.removeEventListener('mouseover', this.#showTooltipHandler.bind(this));
   }
 
   set(flay: Flay, options: Partial<FlayMarkerOptions> = {}): void {
+    document.querySelector('.flay-tooltip')?.remove(); // Remove existing tooltip
+    this.classList.remove('square', 'circle', 'star', 'heart', 'rhombus');
+    this.innerHTML = ''; // Clear previous content
+    this.title = '';
     if (!flay) {
       console.warn('FlayMarker: Flay data is required');
       return;
     }
-    const mergedOptions: FlayMarkerOptions = { ...DEFAULT_OPTIONS, ...options };
+    this.#options = { ...DEFAULT_OPTIONS, ...options };
 
     this.flay = flay;
     this.classList.add('flay-marker');
     this.classList.toggle('shot', flay.video.likes?.length > 0);
     this.classList.toggle('archive', flay.archive);
 
-    this.classList.remove('square', 'circle', 'star', 'heart', 'rhombus');
-    this.innerHTML = ''; // Clear previous content
-    this.title = '';
-
-    if (mergedOptions.showCover) {
-      this.addEventListener('mouseover', this.#mouseoverHandler.bind(this), { once: true });
+    this.classList.add(this.#options.shape);
+    switch (this.#options.shape) {
+      case 'heart':
+        this.innerHTML = favorite;
+        break;
+      case 'star':
+        this.innerHTML = ranks[flay.video.rank + 1];
+        break;
     }
-    if (mergedOptions.showTitle) {
+
+    if (!this.#options.tooltip) {
       this.title = `${flay.studio}\n${flay.opus}\n${flay.title}\n${flay.actressList.join(', ')}\n${flay.release}\n${flay.video.rank}R`;
-    }
-
-    this.classList.add(mergedOptions.shape);
-    if (mergedOptions.shape === 'heart') {
-      this.innerHTML = favorite;
-    } else if (mergedOptions.shape === 'star') {
-      this.innerHTML = ranks[flay.video.rank + 1];
     }
   }
 
@@ -88,21 +88,27 @@ export default class FlayMarker extends HTMLLabelElement {
    * 클릭 이벤트 핸들러
    * - Flay 팝업 표시
    */
-  #clickHandler(): void {
+  #popupFlayHandler(): void {
     popupFlay(this.flay.opus);
     this.classList.add('active');
   }
 
-  #mouseoverHandler(): void {
-    this.showCover();
-  }
-
   /**
-   * 커버 이미지를 배경으로 표시합니다.
+   * 툴팁을 표시합니다.
+   * - 이미지와 주요 정보를 이 마커의 위치에 표시합니다.
    */
-  showCover(): void {
-    this.classList.add('cover');
-    this.style.backgroundImage = `url(${ApiClient.buildUrl(`/static/cover/${this.flay.opus}`)})`;
+  #showTooltipHandler(): Promise<void> {
+    if (!this.#options.tooltip) return;
+
+    const tooltipWidth = StyleUtils.remToPx(10);
+    const { x, y, width, height } = this.getBoundingClientRect();
+    const tooltipX = x + width / 2; // Center the tooltip horizontally
+    const tooltipY = y + height / 2; // Center the tooltip vertically
+
+    const tooltip = (document.querySelector('.flay-tooltip') || document.body.appendChild(new FlayTooltip(tooltipWidth))) as FlayTooltip;
+    tooltip.show(this.flay, tooltipX, tooltipY);
+
+    // this.addEventListener('mouseleave', () => tooltip.hide(), { once: true });
   }
 }
 

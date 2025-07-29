b7be028e6ea269485b034173a0789d2bd7e51bb9
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-29 20:56:30 +0900

    fix: refactor FlayMarkerFloat to use OpusProvider and optimize shape handling

--- www/src/flay/panel/FlayMarkerFloat.ts
+++ www/src/flay/panel/FlayMarkerFloat.ts
@@ -1,13 +1,14 @@
-import FlayMarker, { ShapeType } from '@flay/domain/FlayMarker';
+import FlayMarker, { SHAPES, ShapeType } from '@flay/domain/FlayMarker';
 import FlayFetch, { Flay } from '@lib/FlayFetch';
 import StyleUtils from '@lib/StyleUtils';
 import './FlayMarkerFloat.scss';
+import { OpusProvider } from '../../lib/OpusProvider';
 
 export class FlayMarkerFloat extends HTMLDivElement {
-  #flayMarker: FlayMarker | null = null;
-  #opusList: string[] = [];
   #intervalIdOfMarker: number | null = null;
   #intervalIdOfShape: number | null = null;
+  #flayMarker = new FlayMarker(null, {});
+  #opusProvider = new OpusProvider();
 
   constructor() {
     super();
@@ -15,34 +16,33 @@ export class FlayMarkerFloat extends HTMLDivElement {
   }
 
   connectedCallback(): void {
+    this.appendChild(this.#flayMarker);
     this.#start();
   }
 
   disconnectedCallback(): void {
+    this.#flayMarker.remove();
     clearInterval(this.#intervalIdOfMarker);
     clearInterval(this.#intervalIdOfShape);
-    this.querySelectorAll('.flay-marker').forEach((marker) => marker.remove());
   }
 
-  async #start(): Promise<void> {
-    this.#opusList = await FlayFetch.getOpusList({});
-    this.#flayMarker = new FlayMarker(null, {});
-    this.appendChild(this.#flayMarker);
-    await this.#updateMarker();
+  #start(): void {
+    this.#updateMarker();
     this.#intervalIdOfMarker = window.setInterval(() => this.#updateMarker(), 1000 * 60); // Refresh every 1 minute
     this.#intervalIdOfShape = window.setInterval(() => this.#updateShape(), 1000 * 10); // Change shape every 10 seconds
   }
 
-  async #updateMarker(): Promise<void> {
-    const { randomFlay, randomRem, randomX, randomY, shape } = await this.#getRandomInfo();
-    this.style.setProperty('--marker-size', `${randomRem}rem`);
-    this.style.setProperty('--shot-blur', `${randomRem * 0.75}rem`);
-    this.style.setProperty('--shot-spread', `${randomRem * 0.5}rem`);
-    this.style.setProperty('--square-radius', `${randomRem * 0.25}rem`);
+  #updateMarker(): void {
+    this.#getRandomInfo().then(({ randomFlay, randomRem, randomX, randomY, shape }) => {
+      this.style.setProperty('--marker-size', `${randomRem}rem`);
+      this.style.setProperty('--shot-blur', `${randomRem * 0.75}rem`);
+      this.style.setProperty('--shot-spread', `${randomRem * 0.5}rem`);
+      this.style.setProperty('--square-radius', `${randomRem * 0.25}rem`);
 
-    this.#flayMarker.set(randomFlay, { tooltip: true, shape: shape });
-    this.#flayMarker.style.left = `${randomX}px`;
-    this.#flayMarker.style.top = `${randomY}px`;
+      this.#flayMarker.set(randomFlay, { tooltip: true, shape: shape });
+      this.#flayMarker.style.left = `${randomX}px`;
+      this.#flayMarker.style.top = `${randomY}px`;
+    });
   }
 
   #updateShape(): void {
@@ -50,8 +50,7 @@ export class FlayMarkerFloat extends HTMLDivElement {
   }
 
   async #getRandomInfo(): Promise<{ randomFlay: Flay; randomRem: number; randomX: number; randomY: number; shape: ShapeType }> {
-    const randomIndex = Math.floor(Math.random() * this.#opusList.length);
-    const randomOpus = this.#opusList[randomIndex];
+    const randomOpus = await this.#opusProvider.getRandomOpus();
     const randomFlay = await FlayFetch.getFlay(randomOpus);
     const randomRem = Math.floor(Math.random() * 3) + 2; // 2 ~ 4 randomly select a size
     const [randomX, randomY] = this.#randomPosition(randomRem);
@@ -60,15 +59,14 @@ export class FlayMarkerFloat extends HTMLDivElement {
   }
 
   #randomPosition(rem: number): [number, number] {
-    const widthPx = StyleUtils.remToPx(rem); // Marker width
-    const excludesPx = StyleUtils.remToPx(10); // 10rem in pixels
-    const randomX = Math.random() * (this.clientWidth - excludesPx * 2) + excludesPx;
-    const randomY = Math.random() * (this.clientHeight - excludesPx * 2) + excludesPx;
-    return [Math.round(randomX - widthPx / 2), Math.round(randomY - widthPx / 2)]; // Center the marker
+    const [markerSize, edgeSize] = [StyleUtils.remToPx(rem), StyleUtils.remToPx(10)];
+    const randomX = Math.random() * (this.clientWidth - edgeSize * 2) + edgeSize;
+    const randomY = Math.random() * (this.clientHeight - edgeSize * 2) + edgeSize;
+    return [Math.round(randomX - markerSize / 2), Math.round(randomY - markerSize / 2)]; // Center the marker
   }
 
   #randomShape(): ShapeType {
-    return ['square', 'circle', 'star', 'heart', 'rhombus'][Math.floor(Math.random() * 5)] as ShapeType;
+    return SHAPES[Math.floor(Math.random() * SHAPES.length)];
   }
 }
 

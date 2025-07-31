9e8e8f450f8ae3368bfca6279581e2e30ccd82b9
Crazy JK <Crazy.4.JK@gmail.com>
2025-07-31 09:49:19 +0900

    fix: refactor async structure in index.ts for improved readability

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -1,27 +1,26 @@
 import './inc/Page';
 import './index.scss';
 
-(async () => {
-  await import(/* webpackChunkName: "FacadeWebMovie" */ '@/movie/FacadeWebMovie')
-    .then(({ FacadeWebMovie }) => new FacadeWebMovie())
-    .then(async (facadeWebMovie) => {
-      document.querySelector('body > main').appendChild(facadeWebMovie);
-      await facadeWebMovie.isEnded();
-    });
-
-  import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
-    .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
-    .then((imageCircle) => {
-      document.head.appendChild(document.createElement('style')).textContent = `
+await import(/* webpackChunkName: "FacadeWebMovie" */ '@/movie/FacadeWebMovie')
+  .then(({ FacadeWebMovie }) => new FacadeWebMovie())
+  .then(async (facadeWebMovie) => {
+    document.querySelector('body > main').appendChild(facadeWebMovie);
+    await facadeWebMovie.isEnded();
+  })
+  .then(() => {
+    import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
+      .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
+      .then((imageCircle) => {
+        document.head.appendChild(document.createElement('style')).textContent = `
         .image-circle { opacity: 0.5; transition: opacity 0.3s ease-in-out; }
         .image-circle:hover { opacity: 1; }`;
-      imageCircle.classList.add('right-bottom');
-      document.body.appendChild(imageCircle);
-    });
+        imageCircle.classList.add('right-bottom');
+        document.body.appendChild(imageCircle);
+      });
 
-  import(/* webpackChunkName: "FlayMarkerFloat" */ '@flay/panel/FlayMarkerFloat')
-    .then(({ FlayMarkerFloat }) => new FlayMarkerFloat())
-    .then((flayMarkerFloat) => {
-      document.body.appendChild(flayMarkerFloat);
-    });
-})();
+    import(/* webpackChunkName: "FlayMarkerFloat" */ '@flay/panel/FlayMarkerFloat')
+      .then(({ FlayMarkerFloat }) => new FlayMarkerFloat())
+      .then((flayMarkerFloat) => {
+        document.body.appendChild(flayMarkerFloat);
+      });
+  });

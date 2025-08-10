28eeff69b4ee33a7c5de7cfad17558c0d1acf045
Crazy JK <Crazy.4.JK@gmail.com>
2025-08-10 15:28:31 +0900

    fix: null 체크를 위한 단언 연산자 추가

--- www/src/view/page.flay-page.ts
+++ www/src/view/page.flay-page.ts
@@ -4,16 +4,16 @@ import FlayPagination from '@flay/panel/FlayPagination';
 import './inc/Page';
 import './page.flay-page.scss';
 
-const flayCondition = document.querySelector('body > main > header').appendChild(new FlayCondition());
-const flayPagination = document.querySelector('body > main > footer').appendChild(new FlayPagination());
-const flayPage = document.querySelector('body > main > article').appendChild(new FlayPage());
+const flayCondition = document.querySelector('body > main > header')!.appendChild(new FlayCondition());
+const flayPagination = document.querySelector('body > main > footer')!.appendChild(new FlayPagination());
+const flayPage = document.querySelector('body > main > article')!.appendChild(new FlayPage());
 
 flayCondition.addEventListener('fetch', () => flayPagination.set(flayCondition.opusList));
 flayPagination.addEventListener('change', async () => {
   flayPagination.off();
   if (flayPagination.opus) {
     const viewTransition = document.startViewTransition(async () => {
-      const { flay } = await flayPage.set(flayPagination.opus);
+      const { flay } = await flayPage.set(flayPagination.opus!);
       flayPage.classList.toggle('hide', false);
       flayCondition.updateSearchItem(flay);
     });

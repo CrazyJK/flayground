24e13423ace826149662dd505b1afb339009b0f4
Crazy JK <Crazy.4.JK@gmail.com>
2025-08-10 15:06:21 +0900

    feat: ESLint 및 Prettier 설정 개선, TypeScript 관련 규칙 조정 및 사용자 정의 사전 추가

--- www/.eslintrc.js
+++ www/.eslintrc.js
@@ -5,48 +5,9 @@ module.exports = {
     node: true,
     es2022: true,
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  extends: ['eslint:recommended', '@typescript-eslint/recommended', '@typescript-eslint/recommended-requiring-type-checking', 'prettier'],
+  extends: ['eslint:recommended', 'prettier'],
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    // TypeScript specific rules
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-    '@typescript-eslint/explicit-module-boundary-types': 'warn',
-    '@typescript-eslint/no-non-null-assertion': 'warn',
-    '@typescript-eslint/prefer-nullish-coalescing': 'error',
-    '@typescript-eslint/prefer-optional-chain': 'error',
-    '@typescript-eslint/prefer-as-const': 'error',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/await-thenable': 'error',
-    '@typescript-eslint/no-misused-promises': 'error',
-    '@typescript-eslint/require-await': 'error',
-    '@typescript-eslint/prefer-readonly': 'warn',
-    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
-    '@typescript-eslint/prefer-includes': 'error',
-    '@typescript-eslint/no-useless-constructor': 'error',
-    '@typescript-eslint/switch-exhaustiveness-check': 'error',
-
-    // Disable JS rules that are covered by TypeScript
-    'no-unused-vars': 'off',
-    'no-undef': 'off',
-    'no-redeclare': 'off',
-    'no-dupe-class-members': 'off',
-
-    // General rules
-    'no-console': 'warn',
-    'no-debugger': 'error',
-    'prefer-const': 'error',
-    'no-var': 'error',
-
     // Prettier rules
     'prettier/prettier': [
       'error',
@@ -54,6 +15,66 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
+
+    // General rules
+    'no-console': 'off', // 개발 중엔 console 허용
+    'no-debugger': 'error',
+    'prefer-const': 'warn', // error에서 warn으로
+    'no-var': 'error',
+
+    // Disable JS rules that are covered by TypeScript
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'no-redeclare': 'off',
+    'no-dupe-class-members': 'off',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint', 'prettier'],
+      rules: {
+        // TypeScript specific rules
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off', // 너무 엄격함
+        '@typescript-eslint/explicit-module-boundary-types': 'off', // 너무 엄격함
+        '@typescript-eslint/no-non-null-assertion': 'warn',
+        '@typescript-eslint/prefer-nullish-coalescing': 'warn', // error에서 warn으로
+        '@typescript-eslint/prefer-optional-chain': 'warn',
+        '@typescript-eslint/prefer-as-const': 'error',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'warn', // error에서 warn으로
+        '@typescript-eslint/no-floating-promises': 'warn', // error에서 warn으로 (개발 중엔 유연하게)
+        '@typescript-eslint/await-thenable': 'error',
+        '@typescript-eslint/no-misused-promises': 'warn', // error에서 warn으로
+        '@typescript-eslint/require-await': 'warn', // error에서 warn으로
+        '@typescript-eslint/prefer-readonly': 'off', // 너무 많은 경고 발생
+        '@typescript-eslint/prefer-string-starts-ends-with': 'warn',
+        '@typescript-eslint/prefer-includes': 'warn',
+        '@typescript-eslint/no-useless-constructor': 'warn',
+        '@typescript-eslint/switch-exhaustiveness-check': 'error',
+
+        // Disable conflicting rules
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        'no-redeclare': 'off',
+        'no-dupe-class-members': 'off',
+
+        // Prettier rules
+        'prettier/prettier': [
+          'error',
+          {
+            endOfLine: 'auto',
+          },
+        ],
+      },
+    },
+  ],
   ignorePatterns: ['node_modules/', 'dist/', '**/*.js', '**/*.jsx', 'webpack.*.cjs', 'madge.cjs'],
 };

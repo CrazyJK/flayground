70039a5d0fbe1fe9175c9302b80468d56588efe3
Crazy JK <Crazy.4.JK@gmail.com>
2025-08-11 20:55:02 +0900

    fix: flay-tag 페이지 스타일 및 스크립트 개선

--- www/src/view/page.flay-tag.ts
+++ www/src/view/page.flay-tag.ts
@@ -8,7 +8,7 @@ const tagContainer = document.querySelector('body > header > div')!;
 const markerContainer = document.querySelector('body > main')!;
 const gridContainer = document.querySelector('body > footer')!;
 
-const excludeTagGroup = ['grade', 'screen', 'etc'];
+gridContainer.appendChild(new GridControl('body > main'));
 
 tagContainer.addEventListener('change', () => {
   const selectedTags = Array.from(tagContainer.querySelectorAll('input[type="checkbox"]:checked'));
@@ -20,11 +20,11 @@ tagContainer.addEventListener('change', () => {
     })
   )
     .then((results) => {
-      markerContainer.innerHTML = ''; // 기존 콘텐츠 제거
       const flays = results.flat(); // 다수의 결과를 하나로 합침
       // flays 중복 제거
       const uniqueFlays = Array.from(new Set(flays.map((flay) => flay.opus))).map((opus) => flays.find((flay) => flay.opus === opus)!);
 
+      markerContainer.innerHTML = ''; // 기존 콘텐츠 제거
       markerContainer.append(...uniqueFlays.map((flay) => new FlayMarker(flay, { cover: true })));
     })
     .catch((error) => {
@@ -34,29 +34,26 @@ tagContainer.addEventListener('change', () => {
 
 FlayFetch.getTags()
   .then((tags) => {
-    console.log(tags);
+    const excludeTagGroup = ['grade', 'screen', 'etc'];
 
+    const fragment = document.createDocumentFragment();
     tags
-      .filter((tag) => {
-        return tag.group && !excludeTagGroup.includes(tag.group);
-      })
-      .sort((t1, t2) => {
-        return t1.group.localeCompare(t2.group);
-      })
+      .filter((tag) => tag.group && !excludeTagGroup.includes(tag.group))
+      .sort((t1, t2) => t1.group.localeCompare(t2.group))
       .forEach((tag) => {
         const checkbox = document.createElement('input');
         checkbox.id = `tag-${tag.id}`;
         checkbox.type = 'checkbox';
         checkbox.value = tag.id.toString();
-        tagContainer.appendChild(checkbox);
 
         const label = document.createElement('label');
         label.htmlFor = checkbox.id;
         label.textContent = tag.name;
-        tagContainer.appendChild(label);
+
+        fragment.append(checkbox, label);
       });
 
-    gridContainer.appendChild(new GridControl('body > main'));
+    tagContainer.appendChild(fragment);
   })
   .catch((error) => {
     console.error(error);

b09d3cf74993d0235334fbfc88e2a64721ea090e
Crazy JK <Crazy.4.JK@gmail.com>
2025-08-15 21:52:59 +0900

    fix: FlayTooltip 클래스의 내부 상태 변수 및 메서드 정리

--- www/src/flay/domain/FlayTooltip.ts
+++ www/src/flay/domain/FlayTooltip.ts
@@ -6,26 +6,46 @@ export class FlayTooltip extends GroundFlay {
   #width: number = 300;
   #height: number = 200;
 
+  #cover: HTMLImageElement;
+  #title: HTMLDivElement;
+  #actress: HTMLDivElement;
+  #etc: HTMLDivElement;
+
+  #prevOpus: string | null = null;
+
   constructor(width: number = 300) {
     super();
-    this.#width = width;
-  }
 
-  connectedCallback(): void {
-    this.style.width = `${this.#width}px`;
+    this.#width = width;
     this.innerHTML = `
       <img src="" alt="" />
       <div class="info">
         <div class="title">title</div>
         <div class="actressList">actressList</div>
-        <div class="rank">video.rank</div>
+        <div class="etc">video.rank</div>
       </div>
     `;
+    this.#cover = this.querySelector('img')!;
+    this.#title = this.querySelector('.title')!;
+    this.#actress = this.querySelector('.actressList')!;
+    this.#etc = this.querySelector('.etc')!;
+  }
+
+  connectedCallback(): void {
+    this.style.width = `${this.#width}px`;
     this.#height = this.offsetHeight;
   }
 
-  disconnectedCallback(): void {}
+  disconnectedCallback(): void {
+    FlayFetch.clearAll();
+  }
 
+  /**
+   * Show the tooltip at the specified position.
+   * @param flay
+   * @param x
+   * @param y
+   */
   show(flay: Flay, x: number, y: number): void {
     let [left, top] = [x, y];
     // 화면 오른쪽에 넘치면, 왼쪽으로 이동
@@ -36,16 +56,21 @@ export class FlayTooltip extends GroundFlay {
     if (y + this.#height > window.innerHeight) {
       top = Math.max(0, y - this.#height);
     }
+
+    if (this.#prevOpus !== null) FlayFetch.clear(this.#prevOpus);
     FlayFetch.getCoverURL(flay.opus)
       .then((coverURL) => {
         this.classList.remove('hide');
         this.style.left = `${left}px`;
         this.style.top = `${top}px`;
-        this.querySelector('img')!.src = coverURL;
-        this.querySelector('img')!.alt = flay.title;
-        this.querySelector('.title')!.textContent = flay.title;
-        this.querySelector('.actressList')!.textContent = flay.actressList.join(', ');
-        this.querySelector('.rank')!.textContent = `${flay.video.rank}R - ${flay.release}`;
+        this.#cover.src = coverURL;
+        this.#cover.alt = `${flay.opus} ${flay.title}`;
+        this.#title.innerHTML = flay.title;
+        this.#actress.innerHTML = flay.actressList.join(', ');
+        this.#etc.innerHTML = `${flay.video.rank}R - ${flay.release} - ${flay.video.likes?.length || 0}S`;
+      })
+      .then(() => {
+        this.#prevOpus = flay.opus;
       })
       .catch((error) => {
         console.error('FlayTooltip: Error fetching cover URL:', error);

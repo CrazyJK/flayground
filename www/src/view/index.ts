2f00add73427c0ccce8a2c0679a276421f59ff64
Crazy JK <Crazy.4.JK@gmail.com>
2025-08-23 16:10:14 +0900

    feat: 이미지 원형 표시 컴포넌트에 스타일 추가 기능을 유틸리티 함수로 변경

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -18,9 +18,9 @@ void import(/* webpackChunkName: "FacadeWebMovie" */ '@movie/FacadeWebMovie')
 void import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
   .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
   .then((imageCircle) => {
-    document.head.appendChild(document.createElement('style')).textContent = `
-        .image-circle { opacity: 0.5; transition: opacity 0.3s ease-in-out; }
-        .image-circle:hover { opacity: 1; }`;
+    imageCircle.addExtraStyles(`
+        image-circle { opacity: 0.5; transition: opacity 0.3s ease-in-out; }
+        image-circle:hover { opacity: 1; }`);
     imageCircle.classList.add('right-bottom');
     document.body.appendChild(imageCircle);
   });

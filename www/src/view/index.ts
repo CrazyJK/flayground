8d3dca41c0c198caa48da256dd5d02bc0d0ddb2c
Crazy JK <Crazy.4.JK@gmail.com>
2025-08-26 22:21:17 +0900

    refactor: FacadeWebMovie 및 ImageCircle 임포트 로직 개선

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -5,11 +5,10 @@ import './index.scss';
 
 void import(/* webpackChunkName: "FacadeWebMovie" */ '@movie/FacadeWebMovie')
   .then(({ FacadeWebMovie }) => new FacadeWebMovie())
+  .then((facadeWebMovie) => document.querySelector('body > main')!.appendChild(facadeWebMovie))
   .then(async (facadeWebMovie) => {
-    document.querySelector('body > main')!.appendChild(facadeWebMovie);
     await facadeWebMovie.isEnded();
-  })
-  .then(() => {
+
     void import(/* webpackChunkName: "FlayMarkerFloat" */ '@flay/panel/FlayMarkerFloat')
       .then(({ FlayMarkerFloat }) => new FlayMarkerFloat())
       .then((flayMarkerFloat) => document.body.appendChild(flayMarkerFloat))
@@ -23,19 +22,20 @@ void import(/* webpackChunkName: "FacadeWebMovie" */ '@movie/FacadeWebMovie')
           }
         });
       });
+
+    void import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
+      .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
+      .then((imageCircle) => document.body.appendChild(imageCircle))
+      .then((imageCircle) => {
+        imageCircle.classList.add('right-bottom');
+        imageCircle.addExtraStyles(
+          `image-circle { opacity: 0.5; transition: opacity 0.3s ease-in-out; }
+          image-circle:hover { opacity: 1; }`
+        );
+      });
   })
   .then(() => {
     setTimeout(showLikesList, 1000);
-  });
-
-void import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
-  .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
-  .then((imageCircle) => {
-    imageCircle.addExtraStyles(`
-        image-circle { opacity: 0.5; transition: opacity 0.3s ease-in-out; }
-        image-circle:hover { opacity: 1; }`);
-    imageCircle.classList.add('right-bottom');
-    document.body.appendChild(imageCircle);
   });
 
 const showLikesList = () => {

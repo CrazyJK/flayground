1cb56931798c21f66dcdbf344a324726d8cf3aeb
Crazy JK <Crazy.4.JK@gmail.com>
2025-08-26 22:06:43 +0900

    feat: 좋아요 목록을 표시하는 기능 추가 및 FlayMarkerFloat 이벤트 처리 개선

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -1,3 +1,5 @@
+import FlayFetch from '../lib/FlayFetch';
+import { popupFlay } from '../lib/FlaySearch';
 import './inc/Page';
 import './index.scss';
 
@@ -10,9 +12,20 @@ void import(/* webpackChunkName: "FacadeWebMovie" */ '@movie/FacadeWebMovie')
   .then(() => {
     void import(/* webpackChunkName: "FlayMarkerFloat" */ '@flay/panel/FlayMarkerFloat')
       .then(({ FlayMarkerFloat }) => new FlayMarkerFloat())
+      .then((flayMarkerFloat) => document.body.appendChild(flayMarkerFloat))
       .then((flayMarkerFloat) => {
-        document.body.appendChild(flayMarkerFloat);
+        flayMarkerFloat.addEventListener('changeFlay', (event) => {
+          const { randomFlay } = (event as CustomEvent).detail;
+          const element = document.body.querySelector('[data-opus="' + randomFlay.opus + '"]');
+          if (element) {
+            element?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            element?.classList.add('highlight');
+          }
+        });
       });
+  })
+  .then(() => {
+    setTimeout(showLikesList, 1000);
   });
 
 void import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
@@ -24,3 +37,65 @@ void import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
     imageCircle.classList.add('right-bottom');
     document.body.appendChild(imageCircle);
   });
+
+const showLikesList = () => {
+  void FlayFetch.getFlayAll()
+    .then((flays) => {
+      return flays
+        .filter((flay) => flay.video.rank > 0 && flay.video.likes?.length > 0)
+        .sort((f1, f2) => {
+          const rankDiff = f2.video.rank - f1.video.rank;
+          if (rankDiff !== 0) return rankDiff;
+          const likeDiff = (f2.video.likes?.length || 0) - (f1.video.likes?.length || 0);
+          if (likeDiff !== 0) return likeDiff;
+          const playDiff = f2.video.play - f1.video.play;
+          if (playDiff !== 0) return playDiff;
+          return 0;
+        });
+    })
+    .then((flays) => {
+      const makeLi = (no: number | string, opus: string, title: string, actress: string[], rank: number | string, likes: number | string, plays: number | string) => {
+        return `
+          <span class="no">${no}</span>
+          <span class="opus">${opus}</span>
+          <span class="title">${title}</span>
+          <span class="actress">${actress.join(', ')}</span>
+          <span class="rank">${rank}</span>
+          <span class="likes">${likes}</span>
+          <span class="plays">${plays}</span>
+        `;
+      };
+
+      const fragment = document.createDocumentFragment();
+      const ul = document.querySelector('body > main > #likesList')!.appendChild(document.createElement('ul'));
+
+      const li = document.createElement('li');
+      li.innerHTML = makeLi('No', 'Opus', 'Title', ['Actress'], 'Rank', 'Likes', 'Plays');
+      li.classList.add('header');
+      fragment.appendChild(li);
+
+      let idx = 0;
+      for (const flay of flays) {
+        const playCount = flay.video.play;
+        const likeCount = flay.video.likes.length;
+
+        const li = document.createElement('li');
+        li.innerHTML = makeLi(++idx, flay.opus, flay.title, flay.actressList, flay.video.rank, likeCount, playCount);
+        li.dataset.opus = flay.opus;
+        fragment.appendChild(li);
+
+        // if (likeCount === 1) {
+        //   await FlayAction.setRank(flay.opus, flay.video.rank - 1);
+        // }
+      }
+      ul.appendChild(fragment);
+
+      ul.addEventListener('click', (event) => {
+        const target = event.target as HTMLElement;
+        if (target.closest('li')) {
+          const opus = target.closest('li')!.dataset.opus!;
+          popupFlay(opus);
+        }
+      });
+    });
+};

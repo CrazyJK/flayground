47a43e8cf55106c5650780ac2eb7dd16c4206b2c
Crazy JK <Crazy.4.JK@gmail.com>
2025-08-31 13:35:36 +0900

    feat: index.html 및 index.scss에서 불필요한 요소 제거 및 레이아웃 수정, index.ts에서 FacadeWebMovie 추가 로직 개선

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -3,7 +3,7 @@ import './index.scss';
 
 import(/* webpackChunkName: "FacadeWebMovie" */ '@movie/FacadeWebMovie')
   .then(({ FacadeWebMovie }) => new FacadeWebMovie())
-  .then((facadeWebMovie) => document.querySelector('body > main')!.appendChild(facadeWebMovie))
+  .then((facadeWebMovie) => document.body.appendChild(facadeWebMovie))
   .then((facadeWebMovie) => facadeWebMovie.isEnded())
   .catch(console.error)
   .finally(() => {
@@ -14,9 +14,19 @@ import(/* webpackChunkName: "FacadeWebMovie" */ '@movie/FacadeWebMovie')
 
     import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
       .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
+      .then((imageCircle) =>
+        imageCircle.addExtraStyle(`
+          image-circle {
+            position: fixed;
+            right: 0;
+            bottom: 0;
+            opacity: 0.5;
+            transition: opacity 0.3s ease-in-out;
+          }
+          image-circle:hover {
+            opacity: 1;
+          }`)
+      )
       .then((imageCircle) => document.body.appendChild(imageCircle))
-      .then((imageCircle) => {
-        imageCircle.addClass('right-bottom').addExtraStyle(`image-circle { opacity: 0.5; transition: opacity 0.3s ease-in-out; } image-circle:hover { opacity: 1; }`);
-      })
       .catch(console.error);
   });

63e1ad0c9764c9cb2970b261fa0434a48e891cb7
Crazy JK <Crazy.4.JK@gmail.com>
2026-01-24 15:19:48 +0900

    feat: 'FlayMarkerFloat' 및 'ImageCircle' 컴포넌트 동적 로딩 추가

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -7,26 +7,5 @@ import(/* webpackChunkName: "FacadeWebMovie" */ '@movie/FacadeWebMovie')
   .then((facadeWebMovie) => facadeWebMovie.isEnded())
   .catch(console.error)
   .finally(() => {
-    import(/* webpackChunkName: "FlayMarkerFloat" */ '@flay/panel/FlayMarkerFloat')
-      .then(({ FlayMarkerFloat }) => new FlayMarkerFloat())
-      .then((flayMarkerFloat) => document.body.appendChild(flayMarkerFloat))
-      .catch(console.error);
-
-    import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
-      .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
-      .then((imageCircle) =>
-        imageCircle.addExtraStyle(`
-          image-circle {
-            position: fixed;
-            right: 0;
-            bottom: 0;
-            opacity: 0.5;
-            transition: opacity 0.3s ease-in-out;
-          }
-          image-circle:hover {
-            opacity: 1;
-          }`)
-      )
-      .then((imageCircle) => document.body.appendChild(imageCircle))
-      .catch(console.error);
+    // Application is initialized
   });

b47df24bf430d41f2229cda043754a0745ddf7b1
Crazy JK <Crazy.4.JK@gmail.com>
2026-02-18 17:38:12 +0900

    fix: 비디오 초기화 시 기본 옵션 설정 - 볼륨, 크기 및 종료 행동 추가

--- www/src/view/index.ts
+++ www/src/view/index.ts
@@ -2,7 +2,7 @@ import './inc/Page';
 import './index.scss';
 
 import(/* webpackChunkName: "FacadeWebMovie" */ '@movie/FacadeWebMovie')
-  .then(({ FacadeWebMovie }) => new FacadeWebMovie())
+  .then(({ FacadeWebMovie }) => new FacadeWebMovie({ volume: 0.3, size: '75%', endedBehavior: 'pause' }))
   .then((facadeWebMovie) => document.body.appendChild(facadeWebMovie))
   .then((facadeWebMovie) => facadeWebMovie.isEnded())
   .catch(console.error)

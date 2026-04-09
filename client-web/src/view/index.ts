f5bce324099207bb86e75d37b8172b8290536076
Crazy JK <Crazy.4.JK@gmail.com>
2026-04-09 21:51:07 +0900

    feat: 이미지 원형 컴포넌트 로딩 코드 제거

--- client-web/src/view/index.ts
+++ client-web/src/view/index.ts
@@ -1,20 +1,2 @@
 import './inc/Page';
 import './index.scss';
-
-import(/* webpackChunkName: "ImageCircle" */ '@image/ImageCircle')
-  .then(({ ImageCircle }) => new ImageCircle({ rem: 10, duration: 2000, eventAllow: true }))
-  .then((imageCircle) =>
-    imageCircle.addExtraStyle(`
-      image-circle {
-        position: fixed;
-        right: 0;
-        bottom: 0;
-        opacity: 0.5;
-        transition: opacity 0.3s ease-in-out;
-      }
-      image-circle:hover {
-        opacity: 1;
-      }`)
-  )
-  .then((imageCircle) => document.body.appendChild(imageCircle))
-  .catch(console.error);
